4531222f690e711d4f3e54c6dbe52c040d7457e2
Amarnath Valluri <amarnath.valluri@intel.com>
2018-10-26 14:58:19 +0300

    An interface to support different pmem device manager implementations
    Current code is using two variants(lvm,ndctl) for managing(create,delete)
    underlined pmem devices. The better solution would be to define a generic
    interface , which should be implemented by different implementations.
    
    Signed-off-by: Amarnath Valluri <amarnath.valluri@intel.com>

---
+++ pkg/pmem-device-manager/pmd-manager.go
@@ -0,0 +1,33 @@
+package pmdmanager
+
+//PmemDeviceInfo represents a block device
+type PmemDeviceInfo struct {
+	//Name name of the block device
+	Name string
+	//Path actual device path
+	Path string
+	//Size size allocated for block device
+	Size uint64
+}
+
+//PmemDeviceManager interface to manage the PMEM block devices
+type PmemDeviceManager interface {
+	//GetCapacity returns the available maximum capacity that can be assigned to a Device/Volume
+	GetCapacity() (uint64, error)
+
+	//CreateDevice creates a new block device with give name and size
+	CreateDevice(name string, size uint64) error
+
+	//GetDevice returns the block device information for given name
+	GetDevice(name string) (PmemDeviceInfo, error)
+
+	//DeleteDevice deletes an existing block device with give name.
+	// If 'flush' is 'true', then the device data is zerod beofore deleting the device
+	DeleteDevice(name string, flush bool) error
+
+	//FlushDeviceData zeros all blocks in the blocke device with given name
+	FlushDeviceData(name string) error
+
+	//ListDevices returns all the block devices information that was created by this device manager
+	ListDevices() ([]PmemDeviceInfo, error)
+}
