8120bd5d82df3f2b2665b443532c02571165b6e5
Amarnath Valluri <amarnath.valluri@intel.com>
2018-10-26 15:03:19 +0300

    Initial implementation of ndctl device manager
    Signed-off-by: Amarnath Valluri <amarnath.valluri@intel.com>

---
+++ pkg/pmem-device-manager/pmd-ndctl.go
@@ -0,0 +1,90 @@
+package pmdmanager
+
+import (
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/intel/pmem-csi/pkg/ndctl"
+)
+
+type pmemNdctl struct {
+	ctx *ndctl.Context
+}
+
+var _ PmemDeviceManager = &pmemNdctl{}
+
+//NewPmemDeviceManagerNdctl Instantiates a new ndctl based pmem device manager
+func NewPmemDeviceManagerNdctl() (PmemDeviceManager, error) {
+	ctx, err := ndctl.NewContext()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to initialize pmem context: %s", err.Error())
+	}
+	return &pmemNdctl{
+		ctx: ctx,
+	}, nil
+}
+
+func (pmem *pmemNdctl) GetCapacity() (uint64, error) {
+	var capacity uint64
+	for _, bus := range pmem.ctx.GetBuses() {
+		for _, r := range bus.ActiveRegions() {
+			available := r.MaxAvailableExtent()
+			if available > capacity {
+				capacity = available
+			}
+		}
+	}
+	return capacity, nil
+}
+
+func (pmem *pmemNdctl) CreateDevice(name string, size uint64) error {
+	ns, err := pmem.ctx.CreateNamespace(ndctl.CreateNamespaceOpts{
+		Name: name,
+		Size: size,
+	})
+	if err != nil {
+		return err
+	}
+	data, _ := ns.MarshalJSON() //nolint: gosec
+	glog.Infof("Namespace crated: %v", data)
+
+	return nil
+}
+
+func (pmem *pmemNdctl) DeleteDevice(name string, flush bool) error {
+	if flush {
+		if err := pmem.FlushDeviceData(name); err != nil {
+			glog.Warningf("DeleteDevice: %s\n", err.Error())
+		}
+	}
+	return pmem.ctx.DestroyNamespaceByName(name)
+}
+
+func (pmem *pmemNdctl) FlushDeviceData(name string) error {
+	return fmt.Errorf("Unsupported for pmem devices")
+}
+
+func (pmem *pmemNdctl) GetDevice(name string) (PmemDeviceInfo, error) {
+	ns, err := pmem.ctx.GetNamespaceByName(name)
+	if err != nil {
+		return PmemDeviceInfo{}, err
+	}
+
+	return namespaceToPmemInfo(ns), nil
+}
+
+func (pmem *pmemNdctl) ListDevices() ([]PmemDeviceInfo, error) {
+	devices := []PmemDeviceInfo{}
+	for _, ns := range pmem.ctx.GetActiveNamespaces() {
+		devices = append(devices, namespaceToPmemInfo(ns))
+	}
+	return devices, nil
+}
+
+func namespaceToPmemInfo(ns *ndctl.Namespace) PmemDeviceInfo {
+	return PmemDeviceInfo{
+		Name: ns.Name(),
+		Path: "/dev/" + ns.BlockDeviceName(),
+		Size: ns.Size(),
+	}
+}
