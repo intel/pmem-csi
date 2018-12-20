244228f20cddd2ea500ae5299675b10e322c591f
Amarnath Valluri <amarnath.valluri@intel.com>
2018-12-20 14:31:39 +0200

    unit tests for registry server
    Added unit tests for RegistryServer using ginkgo framework. The tests covers
    registry server basic api and connection setup.
    
    Moved all test* make targets to separate make file.
    
    Signed-off-by: Amarnath Valluri <amarnath.valluri@intel.com>

--- test/setup-ca.sh
+++ test/setup-ca.sh
@@ -1,60 +1,28 @@
-#! /bin/sh -e
+#!/bin/sh
 
-# The default is to build certstrap from source for each invocation,
-# using the source code bundled with OIM, and to put output files
-# into _work/ca.
-: ${CERTSTRAP:=go run $(pwd)/vendor/github.com/square/certstrap/certstrap.go}
+# Directory to use for storing intermediate files
+CA=${CA:="pmem-ca"}
+WORKDIR=${WORKDIR:-$(mktemp -d -u -t pmem-XXXX)}
+mkdir -p $WORKDIR
+cd $WORKDIR
 
-# Default CA name.
-: ${CA:=OIM CA}
+#download cfssl utilities
+cfssl_found=1
+(command -v cfssl 2>&1 >/dev/null && command -v cfssljson 2>&1 >/dev/null) || cfssl_found=0
+if [ $cfssl_found -eq 0 ]; then
+    echo "cfssl tools not found, Please install cfssl, cfssljson."
+    exit 1
+fi
 
-# Default output directory.
-: ${DEPOT_PATH:=_work/ca}
+#Generate CA certificates
+echo '{"CN": "'$CA'", "key": {"algo": "rsa", "size": 2048}}' | cfssl gencert -initca - | cfssljson -bare ca
 
-# Check that it works.
-${CERTSTRAP} --help >/dev/null
-
-CERTSTRAP="${CERTSTRAP} --depot-path ${DEPOT_PATH}"
-
-# For testing purposes it is okay to not use a passphrase.
-${CERTSTRAP} init --common-name "${CA}" --passphrase ""
-
-# These common names have a special meaning in OIM.
-DEFAULT_NAMES="user.admin component.registry"
-
-# Here the "normal" part does not matter, it just has to
-# be something other than "admin".
-DEFAULT_NAMES="${DEFAULT_NAMES} user.normal"
-
-# For each node in the test cluster we need a certificate for the
-# caller and for the recipient of commands. "host-0/1/2/..." is what
-# we use for the first node and also as controller ID in the example
-# deployment of OIM.
-DEFAULT_NAMES="${DEFAULT_NAMES} $(for i in $(seq 0 $((NUM_NODES - 1))); do echo host.host-$i controller.host-$i; done)"
-
-for name in ${NAMES:-${DEFAULT_NAMES}}; do
-    ${CERTSTRAP} request-cert --common-name "$name" --passphrase ""
-    ${CERTSTRAP} sign "$name" --CA "${CA}" --passphrase ""
+# Generate server and client certificates
+DEFAULT_CNS="pmem-registry pmem-node-controller"
+CNS="${DEFAULT_CNS} ${EXTRA_CNS:=""}"
+for name in ${CNS}; do
+  echo '{"CN": "'$name'", "key": {"algo": "ecdsa", "size": 256}}' | \
+  cfssl gencert -ca=ca.pem -ca-key=ca-key.pem - | cfssljson -bare $name
 done
 
-# Now turn all of this into a .yaml file for deployment as "oim-ca" secret.
-
-cat >${DEPOT_PATH}/secret.yaml <<EOF
-apiVersion: v1
-kind: Secret
-metadata:
-  name: oim-ca
-  namespace: default
-type: Opaque
-data:
-EOF
-for name in ${NAMES:-${DEFAULT_NAMES}}; do
-    case $name in
-        host.*)
-            for suffix in crt key; do
-                echo "  $name.$suffix: $(base64 -w 0 ${DEPOT_PATH}/$name.$suffix)" >>${DEPOT_PATH}/secret.yaml
-            done
-            ;;
-    esac
-done
-echo "  ca.crt: $(base64 -w 0 ${DEPOT_PATH}/ca.crt)" >>${DEPOT_PATH}/secret.yaml
+echo "Generated certificates can be located at: $WORKDIR"
