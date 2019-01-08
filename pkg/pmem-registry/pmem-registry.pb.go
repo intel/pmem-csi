14f9f54f5473fe95f3510bb641a64639f812ab0b
Amarnath Valluri <amarnath.valluri@intel.com>
2019-01-08 14:15:04 +0200

    Offload GetCapacity() from RegistryServer to Node ControllerServer
    Only nodes can provide reliable value about 'available capacity' on a node at a
    given point of time. Book-keeping this at registry server on master side is not
    reliable and harder to keep in sync/updated.
    
    So more reliable approach is that Master Controller gets the node capacity from
    Node Controller on need basis. CSI ControllerServer interface already have
    GetCapacity() method, so this change makes use of this API.
    
    Now we can clearly define the use of RegistryServer interface that its only
    responsibility is to maintain a registry of node controller server's endpoints.
    
    So, with this change, Node controller servers implements
    ControllerServer.GetCapacity() and Master controller uses this method to
    determine available capacity by a node.
    
    Updated the sequence diagram to show this additional step.

--- pkg/pmem-registry/pmem-registry.pb.go
+++ pkg/pmem-registry/pmem-registry.pb.go
@@ -4,9 +4,9 @@
 package registry
 
 import (
-	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
 	math "math"
 )
@@ -28,12 +28,10 @@ type RegisterControllerRequest struct {
 	NodeId string `protobuf:"bytes,1,opt,name=node_id,json=nodeId,proto3" json:"node_id,omitempty"`
 	// Node controller's address that can be used for grpc.Dial to
 	// connect to the controller
-	Endpoint string `protobuf:"bytes,2,opt,name=endpoint,proto3" json:"endpoint,omitempty"`
-	// Available capacity of the node.
-	Capacity             map[string]uint64 `protobuf:"bytes,3,rep,name=capacity,proto3" json:"capacity,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"varint,2,opt,name=value,proto3"`
-	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
-	XXX_unrecognized     []byte            `json:"-"`
-	XXX_sizecache        int32             `json:"-"`
+	Endpoint             string   `protobuf:"bytes,2,opt,name=endpoint,proto3" json:"endpoint,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
 func (m *RegisterControllerRequest) Reset()         { *m = RegisterControllerRequest{} }
@@ -73,13 +71,6 @@ func (m *RegisterControllerRequest) GetEndpoint() string {
 		return m.Endpoint
 	}
 	return ""
-}
-
-func (m *RegisterControllerRequest) GetCapacity() map[string]uint64 {
-	if m != nil {
-		return m.Capacity
-	}
-	return nil
 }
 
 type RegisterControllerReply struct {
@@ -184,113 +175,30 @@ func (m *UnregisterControllerReply) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_UnregisterControllerReply proto.InternalMessageInfo
 
-type GetCapacityRequest struct {
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *GetCapacityRequest) Reset()         { *m = GetCapacityRequest{} }
-func (m *GetCapacityRequest) String() string { return proto.CompactTextString(m) }
-func (*GetCapacityRequest) ProtoMessage()    {}
-func (*GetCapacityRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_4bfc8dd910f76aa6, []int{4}
-}
-
-func (m *GetCapacityRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_GetCapacityRequest.Unmarshal(m, b)
-}
-func (m *GetCapacityRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_GetCapacityRequest.Marshal(b, m, deterministic)
-}
-func (m *GetCapacityRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GetCapacityRequest.Merge(m, src)
-}
-func (m *GetCapacityRequest) XXX_Size() int {
-	return xxx_messageInfo_GetCapacityRequest.Size(m)
-}
-func (m *GetCapacityRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_GetCapacityRequest.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_GetCapacityRequest proto.InternalMessageInfo
-
-type GetCapacityReply struct {
-	Capacity             map[string]uint64 `protobuf:"bytes,1,rep,name=capacity,proto3" json:"capacity,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"varint,2,opt,name=value,proto3"`
-	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
-	XXX_unrecognized     []byte            `json:"-"`
-	XXX_sizecache        int32             `json:"-"`
-}
-
-func (m *GetCapacityReply) Reset()         { *m = GetCapacityReply{} }
-func (m *GetCapacityReply) String() string { return proto.CompactTextString(m) }
-func (*GetCapacityReply) ProtoMessage()    {}
-func (*GetCapacityReply) Descriptor() ([]byte, []int) {
-	return fileDescriptor_4bfc8dd910f76aa6, []int{5}
-}
-
-func (m *GetCapacityReply) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_GetCapacityReply.Unmarshal(m, b)
-}
-func (m *GetCapacityReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_GetCapacityReply.Marshal(b, m, deterministic)
-}
-func (m *GetCapacityReply) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GetCapacityReply.Merge(m, src)
-}
-func (m *GetCapacityReply) XXX_Size() int {
-	return xxx_messageInfo_GetCapacityReply.Size(m)
-}
-func (m *GetCapacityReply) XXX_DiscardUnknown() {
-	xxx_messageInfo_GetCapacityReply.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_GetCapacityReply proto.InternalMessageInfo
-
-func (m *GetCapacityReply) GetCapacity() map[string]uint64 {
-	if m != nil {
-		return m.Capacity
-	}
-	return nil
-}
-
 func init() {
 	proto.RegisterType((*RegisterControllerRequest)(nil), "registry.v0.RegisterControllerRequest")
-	proto.RegisterMapType((map[string]uint64)(nil), "registry.v0.RegisterControllerRequest.CapacityEntry")
 	proto.RegisterType((*RegisterControllerReply)(nil), "registry.v0.RegisterControllerReply")
 	proto.RegisterType((*UnregisterControllerRequest)(nil), "registry.v0.UnregisterControllerRequest")
 	proto.RegisterType((*UnregisterControllerReply)(nil), "registry.v0.UnregisterControllerReply")
-	proto.RegisterType((*GetCapacityRequest)(nil), "registry.v0.GetCapacityRequest")
-	proto.RegisterType((*GetCapacityReply)(nil), "registry.v0.GetCapacityReply")
-	proto.RegisterMapType((map[string]uint64)(nil), "registry.v0.GetCapacityReply.CapacityEntry")
 }
 
 func init() { proto.RegisterFile("pmem-registry.proto", fileDescriptor_4bfc8dd910f76aa6) }
 
 var fileDescriptor_4bfc8dd910f76aa6 = []byte{
-	// 340 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x93, 0x51, 0x4b, 0xc2, 0x50,
-	0x14, 0xc7, 0xbd, 0x5a, 0xb6, 0x8e, 0x14, 0x72, 0x12, 0xd4, 0x49, 0x24, 0x23, 0x44, 0x88, 0x46,
-	0x58, 0x44, 0xd4, 0x5b, 0x12, 0xd2, 0x4b, 0xc5, 0xa0, 0x97, 0x5e, 0x62, 0xb9, 0x43, 0xac, 0xe6,
-	0xee, 0xba, 0x5e, 0x85, 0xfb, 0x4d, 0xfa, 0x70, 0x41, 0x5f, 0x25, 0x76, 0x9d, 0xb2, 0x95, 0x9a,
-	0x3d, 0xf4, 0xb6, 0xff, 0xce, 0xf9, 0x9f, 0xff, 0xd9, 0xef, 0x30, 0xd8, 0x89, 0x06, 0x34, 0x38,
-	0x14, 0xf4, 0xec, 0x0f, 0xa5, 0x50, 0x76, 0x24, 0xb8, 0xe4, 0x58, 0x9a, 0xe9, 0xf1, 0x91, 0xf5,
-	0xc9, 0xa0, 0xee, 0x68, 0x4d, 0xa2, 0xcb, 0x43, 0x29, 0x78, 0x10, 0x90, 0x70, 0xe8, 0x6d, 0x44,
-	0x43, 0x89, 0x55, 0xd8, 0x08, 0xb9, 0x47, 0x8f, 0xbe, 0x57, 0x63, 0x4d, 0xd6, 0xde, 0x74, 0x8a,
-	0xb1, 0xbc, 0xf6, 0xd0, 0x04, 0x83, 0x42, 0x2f, 0xe2, 0x7e, 0x28, 0x6b, 0x79, 0x5d, 0x99, 0x69,
-	0xbc, 0x03, 0xa3, 0xef, 0x46, 0x6e, 0xdf, 0x97, 0xaa, 0x56, 0x68, 0x16, 0xda, 0xa5, 0xce, 0x89,
-	0x9d, 0x8a, 0xb4, 0x17, 0xc6, 0xd9, 0xdd, 0xc4, 0x76, 0x15, 0x4a, 0xa1, 0x9c, 0xd9, 0x14, 0xf3,
-	0x02, 0xb6, 0x32, 0x25, 0x2c, 0x43, 0xe1, 0x95, 0x54, 0xb2, 0x53, 0xfc, 0x88, 0x15, 0x58, 0x1f,
-	0xbb, 0xc1, 0x88, 0xf4, 0x36, 0x6b, 0xce, 0x44, 0x9c, 0xe7, 0xcf, 0x98, 0x55, 0x87, 0xea, 0xbc,
-	0xc4, 0x28, 0x50, 0xd6, 0x29, 0x34, 0xee, 0x43, 0xf1, 0xe7, 0xaf, 0xb7, 0x1a, 0x50, 0x9f, 0xef,
-	0x8b, 0x87, 0x56, 0x00, 0x7b, 0x24, 0xa7, 0xfb, 0x26, 0xb3, 0xac, 0x77, 0x06, 0xe5, 0xcc, 0xeb,
-	0x28, 0x50, 0xd8, 0x4b, 0x91, 0x62, 0x9a, 0xd4, 0x41, 0x86, 0xd4, 0x77, 0xc3, 0xbf, 0x00, 0xea,
-	0x7c, 0x30, 0x30, 0x9c, 0x24, 0x15, 0x3d, 0xc0, 0x9f, 0xb4, 0xb0, 0xb5, 0xda, 0x01, 0xcd, 0xfd,
-	0x5f, 0xfb, 0x62, 0x42, 0x39, 0x7c, 0x81, 0xca, 0x3c, 0x80, 0xd8, 0xce, 0xf8, 0x97, 0xdc, 0xc6,
-	0x6c, 0xad, 0xd0, 0xa9, 0xb3, 0x3a, 0x2e, 0x6c, 0xdf, 0x70, 0x8f, 0x52, 0x29, 0xb7, 0x50, 0x4a,
-	0x91, 0xc5, 0xbd, 0xc5, 0xcc, 0x27, 0x59, 0xbb, 0x4b, 0x8f, 0x62, 0xe5, 0x2e, 0xe1, 0xc1, 0x98,
-	0x76, 0x3c, 0x15, 0xf5, 0x4f, 0x76, 0xfc, 0x15, 0x00, 0x00, 0xff, 0xff, 0xe7, 0x94, 0xe8, 0xcc,
-	0x7b, 0x03, 0x00, 0x00,
+	// 207 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2e, 0xc8, 0x4d, 0xcd,
+	0xd5, 0x2d, 0x4a, 0x4d, 0xcf, 0x2c, 0x2e, 0x29, 0xaa, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
+	0xe2, 0x86, 0xf3, 0xcb, 0x0c, 0x94, 0x02, 0xb8, 0x24, 0x83, 0xc0, 0xdc, 0xd4, 0x22, 0xe7, 0xfc,
+	0xbc, 0x92, 0xa2, 0xfc, 0x9c, 0x9c, 0xd4, 0xa2, 0xa0, 0xd4, 0xc2, 0xd2, 0xd4, 0xe2, 0x12, 0x21,
+	0x71, 0x2e, 0xf6, 0xbc, 0xfc, 0x94, 0xd4, 0xf8, 0xcc, 0x14, 0x09, 0x46, 0x05, 0x46, 0x0d, 0xce,
+	0x20, 0x36, 0x10, 0xd7, 0x33, 0x45, 0x48, 0x8a, 0x8b, 0x23, 0x35, 0x2f, 0xa5, 0x20, 0x3f, 0x33,
+	0xaf, 0x44, 0x82, 0x09, 0x2c, 0x03, 0xe7, 0x2b, 0x49, 0x72, 0x89, 0x63, 0x33, 0xb1, 0x20, 0xa7,
+	0x52, 0xc9, 0x8c, 0x4b, 0x3a, 0x34, 0xaf, 0x88, 0x64, 0xeb, 0x94, 0xa4, 0xb9, 0x24, 0xb1, 0xeb,
+	0x2b, 0xc8, 0xa9, 0x34, 0xba, 0xc3, 0xc8, 0xc5, 0x11, 0x04, 0xf5, 0x91, 0x50, 0x0a, 0x97, 0x10,
+	0xa6, 0xe5, 0x42, 0x6a, 0x7a, 0x48, 0x5e, 0xd6, 0xc3, 0xe9, 0x5f, 0x29, 0x15, 0x82, 0xea, 0x40,
+	0xbe, 0x60, 0x10, 0xca, 0xe2, 0x12, 0xc1, 0xe6, 0x1e, 0x21, 0x0d, 0x14, 0xfd, 0x78, 0xbc, 0x2a,
+	0xa5, 0x46, 0x84, 0x4a, 0xb0, 0x5d, 0x4e, 0x5c, 0x51, 0x1c, 0x30, 0xa5, 0x49, 0x6c, 0xe0, 0x08,
+	0x34, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x06, 0x3f, 0x1d, 0x9b, 0xd7, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -392,70 +300,6 @@ var _Registry_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "UnregisterController",
 			Handler:    _Registry_UnregisterController_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "pmem-registry.proto",
-}
-
-// NodeControllerClient is the client API for NodeController service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type NodeControllerClient interface {
-	GetCapacity(ctx context.Context, in *GetCapacityRequest, opts ...grpc.CallOption) (*GetCapacityReply, error)
-}
-
-type nodeControllerClient struct {
-	cc *grpc.ClientConn
-}
-
-func NewNodeControllerClient(cc *grpc.ClientConn) NodeControllerClient {
-	return &nodeControllerClient{cc}
-}
-
-func (c *nodeControllerClient) GetCapacity(ctx context.Context, in *GetCapacityRequest, opts ...grpc.CallOption) (*GetCapacityReply, error) {
-	out := new(GetCapacityReply)
-	err := c.cc.Invoke(ctx, "/registry.v0.NodeController/GetCapacity", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// NodeControllerServer is the server API for NodeController service.
-type NodeControllerServer interface {
-	GetCapacity(context.Context, *GetCapacityRequest) (*GetCapacityReply, error)
-}
-
-func RegisterNodeControllerServer(s *grpc.Server, srv NodeControllerServer) {
-	s.RegisterService(&_NodeController_serviceDesc, srv)
-}
-
-func _NodeController_GetCapacity_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetCapacityRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(NodeControllerServer).GetCapacity(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/registry.v0.NodeController/GetCapacity",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(NodeControllerServer).GetCapacity(ctx, req.(*GetCapacityRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _NodeController_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "registry.v0.NodeController",
-	HandlerType: (*NodeControllerServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "GetCapacity",
-			Handler:    _NodeController_GetCapacity_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
