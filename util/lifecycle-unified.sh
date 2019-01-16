965067de7ffeb8ede10aa3f529c2855df93e2815
Olev Kartau <olev.kartau@intel.com>
2019-01-16 10:40:20 +0200

    util/ scripts changes to support direct-nvdimm mode
    After recent cleanup in driver, driver does not create directory,
    means we need to paths tester script.
    lifecycle script: make size larger 1G, change default erase=true.
    Unified mode test: move run_driver into util, create direct-nvdimm variant
    renamed scripts to express the devicemode in script name.

--- util/lifecycle-unified.sh
+++ util/lifecycle-unified.sh
@@ -5,17 +5,22 @@
 
 export CSI_ENDPOINT=tcp://127.0.0.1:10000
 NAME=nspace5
-STAGEPATH=/tmp/stage5
-TARGETPATH=/tmp/target5
-SIZE=33554432
-FSTYPE=xfs
+SIZE=1073741824
+#ERASE=false
+ERASE=true
+#FSTYPE=xfs
+FSTYPE=ext4
 NSMODE=fsdax
-out=`$GOPATH/bin/csc controller create-volume --req-bytes $SIZE --cap SINGLE_NODE_WRITER,mount,$FSTYPE --params eraseafter=false --params nsmode=$NSMODE $NAME`
+STAGEPATH=/tmp/stage-${NAME}
+TARGETPATH=/tmp/target-${NAME}
+
+out=`$GOPATH/bin/csc controller create-volume --req-bytes $SIZE --cap SINGLE_NODE_WRITER,mount,$FSTYPE --params eraseafter=$ERASE --params nsmode=$NSMODE $NAME`
 VOLID=`echo $out |awk '{print $1}' |tr -d \"`
-#echo [$VOLID]
-mkdir -p $STAGEPATH
+#echo VolumeID=$VOLID
+mkdir -p $STAGEPATH $TARGETPATH
 $GOPATH/bin/csc node stage $VOLID --cap SINGLE_NODE_WRITER,mount,$FSTYPE --staging-target-path $STAGEPATH --attrib name=$NAME --attrib nsmode=$NSMODE
 $GOPATH/bin/csc node publish $VOLID --cap SINGLE_NODE_WRITER,mount,$FSTYPE --staging-target-path $STAGEPATH --target-path $TARGETPATH
 $GOPATH/bin/csc node unpublish $VOLID --target-path $TARGETPATH
 $GOPATH/bin/csc node unstage $VOLID --staging-target-path $STAGEPATH
 $GOPATH/bin/csc controller delete-volume $VOLID
+rmdir $STAGEPATH $TARGETPATH
