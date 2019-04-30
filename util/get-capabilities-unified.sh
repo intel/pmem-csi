36e36560ed22bbe0a2ff91b05b2baab785ecfb16
Olev Kartau <olev.kartau@intel.com>
2019-04-30 14:44:41 +0300

    Removed util/ directory with Unified-mode helper scripts

--- util/get-capabilities-unified.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xeu
-
-# Check driver-reported capabilities
-
-export CSI_ENDPOINT=tcp://127.0.0.1:10000
-$GOPATH/bin/csc controller get-capabilities
-$GOPATH/bin/csc node get-capabilities
