c40aaa0e2f2147467cde2df727cf0641e7b81b71
Olev Kartau <olev.kartau@intel.com>
2019-05-13 15:04:42 +0300

    Drop -namespacesize option, related code, update related doc
    Maintaining multiple fixed-size namespaces does not add value,
    so let's create a single namespace per namespace mode (fsdax, sector).
    Modify DeviceMode diagrams of README to show less namespaces

--- pkg/pmem-ns-init/main_test.go
+++ pkg/pmem-ns-init/main_test.go
@@ -26,37 +26,34 @@ var _ = AfterSuite(func() {
 var _ = Describe("pmem-ns-init", func() {
 	Context("Check arguments", func() {
 		type cases struct {
-			name                                     string
-			namespacesize, useforfsdax, useforsector int
+			name                      string
+			useforfsdax, useforsector int
 		}
 		goodcases := []cases{
-			{"namespacesize ok", 32, 0, 0},
-			{"useforfsdax ok", 32, 50, 0},
-			{"useforsector ok", 32, 0, 50},
-			{"useforfsdax and useforsector combined 100", 32, 70, 30},
-			{"useforfsdax and useforsector combined less than 100", 32, 40, 30},
+			{"useforfsdax ok", 50, 0},
+			{"useforsector ok", 0, 50},
+			{"useforfsdax and useforsector combined 100", 70, 30},
+			{"useforfsdax and useforsector combined less than 100", 40, 30},
 		}
 		badcases := []cases{
-			{"namespacesize negative", -1, 0, 0},
-			{"namespacesize too small", 1, 0, 0},
-			{"useforfsdax negative", 32, -1, 0},
-			{"useforsector negative", 32, 0, -1},
-			{"useforfsdax too large", 32, 101, 0},
-			{"useforsector too large", 32, 0, 101},
-			{"useforfsdax and useforsector combined too large", 32, 51, 51},
+			{"useforfsdax negative", -1, 0},
+			{"useforsector negative", 0, -1},
+			{"useforfsdax too large", 101, 0},
+			{"useforsector too large", 0, 101},
+			{"useforfsdax and useforsector combined too large", 51, 51},
 		}
 
 		for _, c := range goodcases {
 			c := c
 			It(c.name, func() {
-				err := pmemnsinit.CheckArgs(c.namespacesize, c.useforfsdax, c.useforsector)
+				err := pmemnsinit.CheckArgs(c.useforfsdax, c.useforsector)
 				Expect(err).NotTo(HaveOccurred())
 			})
 		}
 		for _, c := range badcases {
 			c := c
 			It(c.name, func() {
-				err := pmemnsinit.CheckArgs(c.namespacesize, c.useforfsdax, c.useforsector)
+				err := pmemnsinit.CheckArgs(c.useforfsdax, c.useforsector)
 				Expect(err).To(HaveOccurred())
 			})
 		}
