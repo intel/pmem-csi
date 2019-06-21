cfa3b7f233c032211d3981f01a6449a2351359cc
Patrick Ohly <patrick.ohly@intel.com>
2019-06-21 14:16:23 +0200

    deploy: enable late binding example
    For Kubernetes 1.14 (and only there) should we show how to try out the
    late binding mode. On 1.13 it doesn't work reliably.
    
    This becomes easier when describing the deployment inside the setup
    script (just one place which needs to be modified, not two).
    
    While at it, the .yaml files also get cleaned up (no empty line at the
    end, same comment as in other .yaml files).

--- test/setup-deployment.sh
+++ test/setup-deployment.sh
@@ -18,9 +18,35 @@ DEPLOYMENT_FILES=(
     pmem-storageclass-ext4.yaml
     pmem-storageclass-xfs.yaml
     pmem-storageclass-cache.yaml
+    pmem-storageclass-late-binding.yaml
 )
 
 echo "$KUBERNETES_VERSION" > $WORK_DIRECTORY/kubernetes.version
 for deployment_file in ${DEPLOYMENT_FILES[@]}; do
-    sed "s|PMEM_REGISTRY|${TEST_PMEM_REGISTRY}|g" ${DEPLOYMENT_DIRECTORY}/${deployment_file} | ${KUBECTL} apply -f -
+    if [ -e ${DEPLOYMENT_DIRECTORY}/${deployment_file} ]; then
+        sed "s|PMEM_REGISTRY|${TEST_PMEM_REGISTRY}|g" ${DEPLOYMENT_DIRECTORY}/${deployment_file} | ${KUBECTL} apply -f -
+    fi
 done
+
+cat <<EOF
+
+The test cluster is ready. Log in with ${WORK_DIRECTORY}/ssh-${CLUSTER}, run kubectl once logged in.
+Alternatively, KUBECONFIG=${WORK_DIRECTORY}/kube.config can also be used directly.
+
+To try out the pmem-csi driver persistent volumes:
+   cat deploy/kubernetes-${KUBERNETES_VERSION}/pmem-pvc.yaml | ${KUBECTL} create -f -
+   cat deploy/kubernetes-${KUBERNETES_VERSION}/pmem-app.yaml | ${KUBECTL} create -f -
+
+To try out the pmem-csi driver cache volumes:
+   cat deploy/kubernetes-${KUBERNETES_VERSION}/pmem-pvc-cache.yaml | ${KUBECTL} create -f -
+   cat deploy/kubernetes-${KUBERNETES_VERSION}/pmem-app-cache.yaml | ${KUBECTL} create -f -
+EOF
+
+if [ -e ${DEPLOYMENT_DIRECTORY}/pmem-storageclass-late-binding.yaml ]; then
+    cat <<EOF
+
+To try out the pmem-csi driver persistent volumes with late binding:
+   cat deploy/kubernetes-${KUBERNETES_VERSION}/pmem-pvc-late-binding.yaml | ${KUBECTL} create -f -
+   cat deploy/kubernetes-${KUBERNETES_VERSION}/pmem-app-late-binding.yaml | ${KUBECTL} create -f -
+EOF
+fi
