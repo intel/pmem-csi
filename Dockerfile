intel/pmem-csi

commit 5c5f60601246865220eeb63ecb7eba7c63f3e15d

Dockerfile: split into three phases
The "build" image is used by the CI for running "make" in a controlled
environment. We can avoid building the driver binaries in that image
to save some time.

Dockerfile
@@ -8,6 +8,9 @@
 ARG CLEAR_LINUX_BASE=clearlinux:latest
 ARG SWUPD_UPDATE_ARG=
 
+# Common base image for building PMEM-CSI:
+# - up-to-date Clear Linux
+# - ndctl installed
 FROM ${CLEAR_LINUX_BASE} AS build
 
 ARG VERSION="unknown"
@@ -30,6 +33,14 @@ RUN ./autogen.sh
 RUN ./configure ${NDCTL_CONFIGFLAGS}
 RUN make install
 
+# Workaround for "error while loading shared libraries: libndctl.so.6" when using older Docker (?)
+# and running "make test" inside this container.
+# - same as https://github.com/clearlinux/distribution/issues/831?
+RUN ldconfig
+
+# Image in which PMEM-CSI binaries get built.
+FROM build as binaries
+
 # build pmem-csi-driver
 ADD . /go/src/github.com/intel/pmem-csi
 ENV GOPATH=/go
@@ -46,12 +57,7 @@ RUN make VERSION=${VERSION} pmem-csi-driver${BIN_SUFFIX} pmem-vgm${BIN_SUFFIX} p
     mv _output/pmem-vgm${BIN_SUFFIX} /go/bin/pmem-vgm && \
     mv _output/pmem-ns-init${BIN_SUFFIX} /go/bin/pmem-ns-init
 
-# Workaround for "error while loading shared libraries: libndctl.so.6" when using older Docker (?)
-# and running "make test" inside this container.
-# - same as https://github.com/clearlinux/distribution/issues/831?
-RUN ldconfig
-
-# build clean container
+# Clean image for deploying PMEM-CSI.
 FROM ${CLEAR_LINUX_BASE}
 LABEL maintainers="Intel"
 LABEL description="PMEM CSI Driver"
@@ -67,10 +73,10 @@ RUN swupd update ${SWUPD_UPDATE_ARG} && swupd bundle-add file xfsprogs storage-u
 RUN ldconfig
 
 # move required binaries and libraries to clean container
-COPY --from=build /usr/lib64/libndctl.so.* /usr/lib/
-COPY --from=build /usr/lib64/libdaxctl.so.* /usr/lib/
+COPY --from=binaries /usr/lib64/libndctl.so.* /usr/lib/
+COPY --from=binaries /usr/lib64/libdaxctl.so.* /usr/lib/
 RUN mkdir -p /go/bin
-COPY --from=build /go/bin/ /go/bin/
+COPY --from=binaries /go/bin/ /go/bin/
 # default lvm config uses lvmetad and throwing below warning for all lvm tools
 # WARNING: Failed to connect to lvmetad. Falling back to device scanning.
 # So, ask lvm not to use lvmetad

commit efd303cd88965ab43208ab511cd9733db718cec9

Dockerfile: Fix missing image version issue
Change 5c5f60601246865220eeb63ecb7eba7c63f3e15d introduced 3-phase image
building, in that VERSION argument spans to different(base & binaries) phases.
This makes VERSION passed via Makefile(build-arg) is not reached properly.

This change fixes this by moving the VERSION argument to appropriate phase.

Dockerfile
@@ -13,7 +13,6 @@ ARG SWUPD_UPDATE_ARG=
 # - ndctl installed
 FROM ${CLEAR_LINUX_BASE} AS build
 
-ARG VERSION="unknown"
 ARG NDCTL_VERSION="65"
 ARG NDCTL_CONFIGFLAGS="--libdir=/usr/lib64 --disable-docs --without-systemd --without-bash"
 ARG NDCTL_BUILD_DEPS="os-core-dev devpkg-util-linux devpkg-kmod devpkg-json-c"
@@ -42,6 +41,7 @@ RUN ldconfig
 FROM build as binaries
 
 # build pmem-csi-driver
+ARG VERSION="unknown"
 ADD . /go/src/github.com/intel/pmem-csi
 ENV GOPATH=/go
 ENV PKG_CONFIG_PATH=/usr/lib/pkgconfig/