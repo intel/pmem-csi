148dc6931d52b8c5e4e6ba48f465b26f6d14c6e0
Patrick Ohly <patrick.ohly@intel.com>
2019-07-31 09:41:58 +0200

    v0.5.0-rc3

--- test/test-config.sh
+++ test/test-config.sh
@@ -56,7 +56,7 @@ fi
 # To use this, do:
 # - make build-images
 # - TEST_CREATE_REGISTRY=true make start
-: ${TEST_BOOTSTRAP_IMAGES:=${TEST_BUILD_PMEM_REGISTRY}/pmem-csi-driver:canary ${TEST_BUILD_PMEM_REGISTRY}/pmem-csi-driver-test:canary}
+: ${TEST_BOOTSTRAP_IMAGES:=${TEST_BUILD_PMEM_REGISTRY}/pmem-csi-driver:v0.5.0-rc3 ${TEST_BUILD_PMEM_REGISTRY}/pmem-csi-driver-test:v0.5.0-rc3
 
 # Additional Clear Linux bundles.
 : ${TEST_CLEAR_LINUX_BUNDLES:=storage-utils}
