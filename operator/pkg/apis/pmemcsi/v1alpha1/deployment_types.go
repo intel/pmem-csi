05a093b4491d5ad2063a4bfea2b179b2ba015ea2
Amarnath Valluri <amarnath.valluri@intel.com>
2019-12-04 09:47:26 +0200

    operator: Initial commit
    This code is generated using operator-sdk. And moved the relevant parts of
    generated operator/go.mod to main go.mode.

---
+++ operator/pkg/apis/pmemcsi/v1alpha1/deployment_types.go
@@ -0,0 +1,51 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DeploymentSpec defines the desired state of Deployment
+// +k8s:openapi-gen=true
+type DeploymentSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// DeploymentStatus defines the observed state of Deployment
+// +k8s:openapi-gen=true
+type DeploymentStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Deployment is the Schema for the deployments API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=deployments,scope=Namespaced
+type Deployment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DeploymentSpec   `json:"spec,omitempty"`
+	Status DeploymentStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DeploymentList contains a list of Deployment
+type DeploymentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Deployment `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Deployment{}, &DeploymentList{})
+}
---
+++ operator/pkg/apis/pmemcsi/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the pmem-csi v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=pmem-csi.intel.com
+package v1alpha1
