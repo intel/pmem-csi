772f5c3c8f39bb1a71f20f27fcc2fb55c66861a9
Amarnath Valluri <amarnath.valluri@intel.com>
2019-12-11 13:06:04 +0200

    operator: Handle new deployment resource.
    Support added to deploy driver upon creation of new deployment custom resource.
    Known Todos:
     - Consider Kubernetes versions
     - Reconcile changes to pre-deployed custom resource
     - Certificates generation

---
+++ operator/pkg/controller/deployment/controller_driver.go
@@ -0,0 +1,627 @@
+package deployment
+
+import (
+	"fmt"
+
+	pmemcsiv1alpha1 "github.com/intel/pmem-csi/operator/pkg/apis/pmemcsi/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	controllerServicePort = 10000
+	nodeControllerPort    = 10001
+)
+
+type PmemCSIDriver struct {
+	pmemcsiv1alpha1.Deployment
+}
+
+func (d *PmemCSIDriver) getDeploymentObjects() []runtime.Object {
+	objects := []runtime.Object{
+		d.getControllerServiceAccount(),
+		d.getControllerProvisionerRole(),
+		d.getControllerProvisionerRoleBinding(),
+		d.getControllerProvisionerClusterRole(),
+		d.getControllerProvisionerClusterRoleBinding(),
+		d.getControllerService(),
+		d.getControllerStatefulSet(),
+		d.getNodeDaemonSet(),
+	}
+
+	return objects
+}
+
+func (d *PmemCSIDriver) getControllerService() *corev1.Service {
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pmem-csi-controller",
+			Namespace: d.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeClusterIP,
+			Ports: []corev1.ServicePort{
+				corev1.ServicePort{
+					Port: controllerServicePort,
+				},
+			},
+			Selector: map[string]string{
+				"app": "pmem-csi-controller",
+			},
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getControllerServiceAccount() *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ServiceAccount",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pmem-csi-controller",
+			Namespace: d.Namespace,
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getControllerProvisionerRole() *rbacv1.Role {
+	return &rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Role",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pmem-csi-provisioner-role",
+			Namespace: d.Namespace,
+		},
+		Rules: []rbacv1.PolicyRule{
+			rbacv1.PolicyRule{
+				APIGroups: []string{""},
+				Resources: []string{"endpoints"},
+				Verbs: []string{
+					"get", "watch", "list", "delete", "update", "create",
+				},
+			},
+			rbacv1.PolicyRule{
+				APIGroups: []string{"coordination.k8s.io"},
+				Resources: []string{"leases"},
+				Verbs: []string{
+					"get", "watch", "list", "delete", "update", "create",
+				},
+			},
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getControllerProvisionerRoleBinding() *rbacv1.RoleBinding {
+	return &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pmem-csi-provisioner-role-binding",
+			Namespace: d.Namespace,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "pmem-csi-controller",
+				Namespace: d.Namespace,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     "pmem-csi-provisioner-role",
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getControllerProvisionerClusterRole() *rbacv1.ClusterRole {
+	return &rbacv1.ClusterRole{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterRole",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "pmem-csi-provisioner-cluster-role",
+		},
+		Rules: []rbacv1.PolicyRule{
+			rbacv1.PolicyRule{
+				APIGroups: []string{""},
+				Resources: []string{"persistentvolumes"},
+				Verbs: []string{
+					"get", "watch", "list", "delete", "create",
+				},
+			},
+			rbacv1.PolicyRule{
+				APIGroups: []string{""},
+				Resources: []string{"persistentvolumes"},
+				Verbs: []string{
+					"get", "watch", "list", "update",
+				},
+			},
+			rbacv1.PolicyRule{
+				APIGroups: []string{"storage.k8s.io"},
+				Resources: []string{"storageclasses"},
+				Verbs: []string{
+					"get", "watch", "list",
+				},
+			},
+			rbacv1.PolicyRule{
+				APIGroups: []string{""},
+				Resources: []string{"events"},
+				Verbs: []string{
+					"watch", "list", "create", "update", "patch",
+				},
+			},
+			rbacv1.PolicyRule{
+				APIGroups: []string{"snapshot.storage.k8s.io"},
+				Resources: []string{"volumesnapshots", "volumesnapshotcontents"},
+				Verbs: []string{
+					"get", "list",
+				},
+			},
+			rbacv1.PolicyRule{
+				APIGroups: []string{"storage.k8s.io"},
+				Resources: []string{"csinodes"},
+				Verbs: []string{
+					"get", "list", "watch",
+				},
+			},
+			rbacv1.PolicyRule{
+				APIGroups: []string{""},
+				Resources: []string{"nodes"},
+				Verbs: []string{
+					"get", "list", "watch",
+				},
+			},
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getControllerProvisionerClusterRoleBinding() *rbacv1.ClusterRoleBinding {
+	return &rbacv1.ClusterRoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterRoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "pmem-csi-provisioner-cluster-role-binding",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "pmem-csi-controller",
+				Namespace: d.Namespace,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "pmem-csi-provisioner-cluster-role",
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getControllerStatefulSet() *appsv1.StatefulSet {
+	replicas := int32(1)
+	ss := &appsv1.StatefulSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "StatefulSet",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pmem-csi-controller",
+			Namespace: d.Namespace,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "pmem-csi-controller",
+				},
+			},
+			ServiceName: "pmem-csi-controller",
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "pmem-csi-controller",
+					},
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: "pmem-csi-controller",
+					Containers: []corev1.Container{
+						d.getControllerContainer(),
+						d.getProvisionerContainer(),
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "plugin-socket-dir",
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+						{
+							Name: "registry-cert",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "pmem-csi-registry-secrets",
+									Items: []corev1.KeyToPath{
+										{
+											Key:  "tls.crt",
+											Path: "pmem-csi-registry.crt",
+										},
+										{
+											Key:  "tls.key",
+											Path: "pmem-csi-registry.key",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return ss
+}
+
+func (d *PmemCSIDriver) getNodeDaemonSet() *appsv1.DaemonSet {
+	directoryOrCreate := corev1.HostPathDirectoryOrCreate
+	ds := &appsv1.DaemonSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "DaemonSet",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pmem-csi-node",
+			Namespace: d.Namespace,
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "pmem-csi-node",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "pmem-csi-node",
+					},
+				},
+				Spec: corev1.PodSpec{
+					NodeSelector: map[string]string{
+						"storage": "pmem",
+					},
+					HostNetwork: true,
+					Containers: []corev1.Container{
+						d.getNodeDriverContainer(),
+						d.getNodeRegistrarContainer(),
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "registration-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/var/lib/kubelet/plugins_registry/",
+									Type: &directoryOrCreate,
+								},
+							},
+						},
+						{
+							Name: "mountpoint-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/var/lib/kubelet/plugins/kubernetes.io/csi",
+									Type: &directoryOrCreate,
+								},
+							},
+						},
+						{
+							Name: "pods-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/var/lib/kubelet/pods",
+									Type: &directoryOrCreate,
+								},
+							},
+						},
+						{
+							Name: "pmem-state-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/var/lib/pmem-csi.intel.com",
+									Type: &directoryOrCreate,
+								},
+							},
+						},
+						{
+							Name: "sys-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/sys",
+									Type: &directoryOrCreate,
+								},
+							},
+						},
+						{
+							Name: "dev-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/dev",
+									Type: &directoryOrCreate,
+								},
+							},
+						},
+						{
+							Name: "registry-cert",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "pmem-csi-node-secrets",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if d.Spec.Node.DeviceMode == "lvm" {
+		ds.Spec.Template.Spec.InitContainers = []corev1.Container{
+			d.getNamespaceInitContainer(),
+			d.getVolumeGroupInitContainer(),
+		}
+	}
+
+	return ds
+}
+
+func (d *PmemCSIDriver) getControllerArgs() []string {
+	args := []string{
+		fmt.Sprintf("-v=%d", d.Spec.LogLevel),
+		"-mode=controller",
+		"-drivername=pmem-csi.intel.com",
+		"-endpoint=unix:///csi/csi-controller.sock",
+		fmt.Sprintf("-registryEndpoint=tcp://0.0.0.0:%d", controllerServicePort),
+		"-nodeid=$(KUBE_NODE_NAME)",
+		"-caFile=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
+		"-certFile=/certs/pmem-csi-registry.crt",
+		"-keyFile=/certs/pmem-csi-registry.key",
+	}
+
+	return args
+}
+
+func (d *PmemCSIDriver) getNodeDriverArgs() []string {
+	args := []string{
+		fmt.Sprintf("-deviceManager=%s", d.Spec.Node.DeviceMode),
+		fmt.Sprintf("-v=%d", d.Spec.LogLevel),
+		"-drivername=pmem-csi.intel.com",
+		"-mode=node",
+		"-endpoint=$(CSI_ENDPOINT)",
+		"-nodeid=$(KUBE_NODE_NAME)",
+		fmt.Sprintf("-controllerEndpoint=tcp://$(KUBE_POD_IP):%d", nodeControllerPort),
+		fmt.Sprintf("-registryEndpoint=$(PMEM_CSI_CONTROLLER_PORT_%d_TCP)", controllerServicePort),
+		"-caFile=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
+		"-statePath=/var/lib/pmem-csi.intel.com",
+		"-certFile=/certs/$(KUBE_NODE_NAME).crt",
+		"-keyFile=/certs/$(KUBE_NODE_NAME).key",
+	}
+
+	return args
+}
+
+func (d *PmemCSIDriver) getControllerContainer() corev1.Container {
+	return corev1.Container{
+		Name:            "pmem-driver",
+		Image:           d.Spec.Image.String(),
+		ImagePullPolicy: d.Spec.Image.PullPolicy,
+		Command:         []string{"/usr/local/bin/pmem-csi-driver"},
+		Args:            d.getControllerArgs(),
+		Env: []corev1.EnvVar{
+			{
+				Name: "KUBE_NODE_NAME",
+				ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{
+						APIVersion: "v1",
+						FieldPath:  "spec.nodeName",
+					},
+				},
+			},
+			{
+				Name:  "TERMINATION_LOG_PATH",
+				Value: "/tmp/termination-log",
+			},
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "registry-cert",
+				MountPath: "/certs",
+			},
+			{
+				Name:      "plugin-socket-dir",
+				MountPath: "/csi",
+			},
+		},
+		Resources: *d.Spec.Controller.Resources,
+	}
+}
+
+func (d *PmemCSIDriver) getNodeDriverContainer() corev1.Container {
+	bidirectional := corev1.MountPropagationBidirectional
+	true := true
+	return corev1.Container{
+		Name:            "pmem-driver",
+		Image:           d.Spec.Image.String(),
+		ImagePullPolicy: d.Spec.Image.PullPolicy,
+		Command:         []string{"/usr/local/bin/pmem-csi-driver"},
+		Args:            d.getNodeDriverArgs(),
+		Env: []corev1.EnvVar{
+			{
+				Name: "KUBE_NODE_NAME",
+				ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{
+						APIVersion: "v1",
+						FieldPath:  "spec.nodeName",
+					},
+				},
+			},
+			{
+				Name: "KUBE_POD_IP",
+				ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{
+						APIVersion: "v1",
+						FieldPath:  "status.podIP",
+					},
+				},
+			},
+			{
+				Name:  "TERMINATION_LOG_PATH",
+				Value: "/tmp/termination-log",
+			},
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:             "mountpoint-dir",
+				MountPath:        "/var/lib/kubelet/plugins/kubernetes.io/csi",
+				MountPropagation: &bidirectional,
+			},
+			{
+				Name:             "pods-dir",
+				MountPath:        "/var/lib/kubelet/pods",
+				MountPropagation: &bidirectional,
+			},
+			{
+				Name:      "registry-cert",
+				MountPath: "/certs",
+			},
+			{
+				Name:      "pmem-state-dir",
+				MountPath: "/var/lib/pmem-csi.intel.com",
+			},
+			{
+				Name:      "dev-dir",
+				MountPath: "/dev",
+			},
+		},
+		Resources: *d.Spec.Node.Resources,
+		SecurityContext: &corev1.SecurityContext{
+			Privileged: &true,
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getProvisionerContainer() corev1.Container {
+	return corev1.Container{
+		Name:            "provisioner",
+		Image:           d.Spec.Controller.ProvisionerImage.String(),
+		ImagePullPolicy: d.Spec.Controller.ProvisionerImage.PullPolicy,
+		Args: []string{
+			"--timeout=5m",
+			fmt.Sprintf("--v=%d", d.Spec.LogLevel),
+			"--csi-address=/csi/csi-controller.sock",
+			"--feature-gates=Topology=true",
+			"--strict-topology=true",
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "plugin-socket-dir",
+				MountPath: "/csi",
+			},
+		},
+		Resources: *d.Spec.Controller.Resources,
+	}
+}
+
+func (d *PmemCSIDriver) getNamespaceInitContainer() corev1.Container {
+	true := true
+	return corev1.Container{
+		Name:            "pmem-ns-init",
+		Image:           d.Spec.Image.String(),
+		ImagePullPolicy: d.Spec.Image.PullPolicy,
+		Command: []string{
+			"/usr/local/bin/pmem-ns-init",
+		},
+		Args: []string{
+			fmt.Sprintf("--v=%d", d.Spec.LogLevel),
+		},
+		Env: []corev1.EnvVar{
+			{
+				Name:  "TERMINATION_LOG_PATH",
+				Value: "/tmp/pmem-ns-init-termination-log",
+			},
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "sys-dir",
+				MountPath: "/sys",
+			},
+		},
+		Resources: *d.Spec.Node.Resources,
+		SecurityContext: &corev1.SecurityContext{
+			Privileged: &true,
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getVolumeGroupInitContainer() corev1.Container {
+	true := true
+	return corev1.Container{
+		Name:            "pmem-vgm",
+		Image:           d.Spec.Image.String(),
+		ImagePullPolicy: d.Spec.Image.PullPolicy,
+		Command: []string{
+			"/usr/local/bin/pmem-vgm",
+		},
+		Args: []string{
+			fmt.Sprintf("--v=%d", d.Spec.LogLevel),
+		},
+		Env: []corev1.EnvVar{
+			{
+				Name:  "TERMINATION_LOG_PATH",
+				Value: "/tmp/pmem-vgm-termination-log",
+			},
+		},
+		Resources: *d.Spec.Node.Resources,
+		SecurityContext: &corev1.SecurityContext{
+			Privileged: &true,
+		},
+	}
+}
+
+func (d *PmemCSIDriver) getNodeRegistrarContainer() corev1.Container {
+	return corev1.Container{
+		Name:            "driver-registrar",
+		Image:           d.Spec.Node.RegistrarImage.String(),
+		ImagePullPolicy: d.Spec.Node.RegistrarImage.PullPolicy,
+		Args: []string{
+			fmt.Sprintf("--v=%d", d.Spec.LogLevel),
+			"--kubelet-registration-path=/var/lib/pmem-csi.intel.com/csi.sock",
+			"--csi-address=/pmem-csi/csi.sock",
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "pmem-state-dir",
+				MountPath: "/pmem-csi",
+			},
+			{
+				Name:      "registration-dir",
+				MountPath: "/registration",
+			},
+		},
+		Resources: *d.Spec.Node.Resources,
+	}
+}
