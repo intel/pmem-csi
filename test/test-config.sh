459828b5f140d0294c3a7db33117e6dd4dacf372
Patrick Ohly <patrick.ohly@intel.com>
2020-01-17 09:41:23 +0100

    test: disable VMX in Jenkins
    The Azure machines that we run the Jenkins jobs on seem to have a
    problem with nested virtualization (`qemu-system-x86_64: error: failed
    to set MSR 0x48b to 0x1582e00000000`,
    https://www.mail-archive.com/qemu-devel@nongnu.org/msg665051.html).
    We don't actually need it right now in the CI, so let's disable it to
    get the CI stable again.

--- test/test-config.sh
+++ test/test-config.sh
@@ -70,6 +70,13 @@ fi
 # Number of CPUS in QEMU VM. Must be at least 2 for Kubernetes.
 : ${TEST_NUM_CPUS:=2}
 
+# QEMU -cpu parameter.
+#
+# "host" enables nested virtualization (required for Kata Containers).
+# The build host must have the kvm_intel module loaded with
+# nested=1 (see https://wiki.archlinux.org/index.php/KVM#Nested_virtualization).
+: ${TEST_QEMU_CPU:=host}
+
 # The etcd instance running on the master node can be configured to
 # store its data on a separate disk. This is the path to an existing
 # file of the desired size which will then be passed into the master
