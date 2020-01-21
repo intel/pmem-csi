4bc60716afe97c39bbf059359f270e9946b8e35d
Amarnath Valluri <amarnath.valluri@intel.com>
2020-01-21 15:17:40 +0200

    operator/deployment: Reconcile deployment changes
    Changes to Deployment object are possible in any phase of the
    deployment. The controller has to reconcile these changes and make sure
    the valid changes are effected to it's sub-resources and invalid
    changes are rejected by reverting the change from deployment object
    stored at api server.
    
    Example invalid changes are:
    - change of device manager mode of a running deployment
    - change of driver name which conflict with other deployment
    
    Unit tests added to test if the deployment changes are handled properly
    by the controller.

--- pkg/pmem-csi-operator/controller/deployment/deployment_controller_test.go
+++ pkg/pmem-csi-operator/controller/deployment/deployment_controller_test.go
@@ -251,6 +251,7 @@ var _ = Describe("Operator", func() {
 				}
 			}
 		})
+
 		It("shall allow deployment with explicit values", func() {
 			d := pmemDeployment{
 				nsn: types.NamespacedName{
@@ -362,6 +363,7 @@ var _ = Describe("Operator", func() {
 				}
 			}
 		})
+
 		It("shall allow multiple deployments", func() {
 			d1 := pmemDeployment{
 				nsn: types.NamespacedName{
@@ -383,8 +385,8 @@ var _ = Describe("Operator", func() {
 			testReconcilePhase(rc, d1.nsn, false, true, api.DeploymentPhasePending)
 
 			testReconcilePhase(rc, d2.nsn, false, true, api.DeploymentPhasePending)
-
 		})
+
 		It("shall not allow multiple deployments with same driver name", func() {
 			d1 := pmemDeployment{
 				nsn: types.NamespacedName{
@@ -423,6 +425,429 @@ var _ = Describe("Operator", func() {
 
 			// After removing first deployment, sencond deployment should progress
 			testReconcilePhase(rc, d2.nsn, false, true, api.DeploymentPhasePending)
+		})
+
+		It("shall allow to change running deployment name", func() {
+			d := pmemDeployment{
+				nsn: types.NamespacedName{
+					Name:      "test-deployment",
+					Namespace: "testnamespace",
+				},
+			}
+
+			rc := getReconciler(getDeployment(d))
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhasePending)
+
+			// Approve certificates
+			for _, name := range []string{"pmem-registry", "pmem-node-controller"} {
+				csrName := d.nsn.Name + "-" + d.nsn.Namespace + "-" + name
+				csr := &certv1beta1.CertificateSigningRequest{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: csrName,
+					},
+				}
+				err := rc.Get(csr)
+				Expect(err).Should(BeNil(), "failed to get csr for %q", csrName)
+				Expect(len(csr.Status.Conditions)).Should(BeEquivalentTo(0), "unexpected status condition")
+
+				csr.Status.Conditions = append(csr.Status.Conditions, certv1beta1.CertificateSigningRequestCondition{
+					Type:           certv1beta1.CertificateApproved,
+					LastUpdateTime: metav1.Now(),
+				})
+
+				csr.Status.Certificate = []byte("Dummy certificate")
+
+				err = rc.Update(csr)
+				Expect(err).Should(BeNil(), "failed to approve CSR")
+			}
+
+			// Reconcile after approved certificates should change the status
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhaseInitializing)
+
+			// Reconcile now should change Phase to running
+			testReconcilePhase(rc, d.nsn, false, false, api.DeploymentPhaseRunning)
+
+			// Ensure both deaemonset and statefulset have default driver name
+			// before editing deployment
+			ss := &appsv1.StatefulSet{}
+			err := rc.Get(ss)
+			Expect(err).Should(BeNil(), "controller stateful set is expected on a successful deployment")
+			for _, c := range ss.Spec.Template.Spec.Containers {
+				if c.Name == "pmem-driver" {
+					Expect(c.Args).Should(ContainElement("-drivername="+api.DefaultDriverName), "mismatched driver name")
+					break
+				}
+			}
+			ds := &appsv1.DaemonSet{}
+			err = rc.Get(ds)
+			Expect(err).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			for _, c := range ds.Spec.Template.Spec.Containers {
+				if c.Name == "pmem-driver" {
+					Expect(c.Args).Should(ContainElement("-drivername="+api.DefaultDriverName), "mismatched driver name")
+					break
+				}
+			}
+
+			// Edit deployment name
+			dep := &api.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      d.nsn.Name,
+					Namespace: d.nsn.Namespace,
+				},
+			}
+			err = rc.Get(dep)
+			Expect(err).Should(BeNil(), "failed to retrieve deployment")
+
+			dep.Spec.DriverName = "test-driver-name"
+			err = rc.Update(dep)
+			Expect(err).Should(BeNil(), "failed to update deployment")
+			testReconcilePhase(rc, d.nsn, false, false, api.DeploymentPhaseRunning)
+
+			// Ensure the new driver name effective in sub resources
+			ss = &appsv1.StatefulSet{}
+			err = rc.Get(ss)
+			Expect(err).Should(BeNil(), "controller stateful set is expected on a successful deployment")
+			for _, c := range ss.Spec.Template.Spec.Containers {
+				if c.Name == "pmem-driver" {
+					Expect(c.Args).Should(ContainElement("-drivername="+dep.Spec.DriverName), "mismatched driver name")
+					break
+				}
+			}
+			ds = &appsv1.DaemonSet{}
+			err = rc.Get(ds)
+			Expect(err).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			for _, c := range ds.Spec.Template.Spec.Containers {
+				if c.Name == "pmem-driver" {
+					Expect(c.Args).Should(ContainElement("-drivername="+dep.Spec.DriverName), "mismatched driver name")
+					break
+				}
+			}
+		})
+
+		It("shall not allow duplicate driver name via update deployment", func() {
+			d1 := pmemDeployment{
+				nsn: types.NamespacedName{
+					Name:      "test-deployment1",
+					Namespace: "testnamespace",
+				},
+				driverName: "test-driver1",
+			}
+
+			d2 := pmemDeployment{
+				nsn: types.NamespacedName{
+					Name:      "test-deployment2",
+					Namespace: "testnamespace",
+				},
+				driverName: "test-driver2",
+			}
+
+			rc := getReconciler(getDeployment(d1), getDeployment(d2))
+			testReconcilePhase(rc, d1.nsn, false, true, api.DeploymentPhasePending)
+			testReconcilePhase(rc, d2.nsn, false, true, api.DeploymentPhasePending)
+
+			// Try to introdue duplicate name when the driver is in Pending phase
+			dep := &api.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      d1.nsn.Name,
+					Namespace: d1.nsn.Namespace,
+				},
+			}
+			err := rc.Get(dep)
+			Expect(err).Should(BeNil(), "failed to retrieve deployment")
+
+			dep.Spec.DriverName = d2.driverName
+			err = rc.Update(dep)
+			Expect(err).Should(BeNil(), "failed to update deployment")
+			testReconcilePhase(rc, d1.nsn, true, true, api.DeploymentPhasePending)
+
+			// Approve certificates for d1, so that it moves to initializing phase
+			csrNames := []string{
+				d1.nsn.Name + "-" + d1.nsn.Namespace + "-" + "pmem-registry",
+				d1.nsn.Name + "-" + d1.nsn.Namespace + "-" + "pmem-node-controller",
+			}
+			for _, csrName := range csrNames {
+				csr := &certv1beta1.CertificateSigningRequest{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: csrName,
+					},
+				}
+				err := rc.Get(csr)
+				Expect(err).Should(BeNil(), "failed to get csr for %q", csrName)
+				Expect(len(csr.Status.Conditions)).Should(BeEquivalentTo(0), "unexpected status condition")
+
+				csr.Status.Conditions = append(csr.Status.Conditions, certv1beta1.CertificateSigningRequestCondition{
+					Type:           certv1beta1.CertificateApproved,
+					LastUpdateTime: metav1.Now(),
+				})
+
+				csr.Status.Certificate = []byte("Dummy certificate")
+
+				err = rc.Update(csr)
+				Expect(err).Should(BeNil(), "failed to approve CSR")
+			}
+
+			// Reconcile after approved certificates should change the status
+			testReconcilePhase(rc, d1.nsn, false, true, api.DeploymentPhaseInitializing)
+
+			// Try to introdue duplicate name when the driver is in Initializing phase
+			dep.Spec.DriverName = d2.driverName
+			err = rc.Update(dep)
+			Expect(err).Should(BeNil(), "failed to update deployment")
+			testReconcilePhase(rc, d1.nsn, true, true, api.DeploymentPhaseInitializing)
+
+			// Move the deployment to Running phase
+			testReconcilePhase(rc, d1.nsn, false, false, api.DeploymentPhaseRunning)
+
+			// Try to introdue duplicate name when the driver is in Running phase
+			dep.Spec.DriverName = d2.driverName
+			err = rc.Update(dep)
+			Expect(err).Should(BeNil(), "failed to update deployment")
+			testReconcilePhase(rc, d1.nsn, true, false, api.DeploymentPhaseRunning)
+		})
+
+		It("shall not allow to change running deployment device mode", func() {
+			d := pmemDeployment{
+				nsn: types.NamespacedName{
+					Name:      "test-deployment",
+					Namespace: "testnamespace",
+				},
+			}
+
+			rc := getReconciler(getDeployment(d))
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhasePending)
+
+			// Approve certificates
+			for _, name := range []string{"pmem-registry", "pmem-node-controller"} {
+				csrName := d.nsn.Name + "-" + d.nsn.Namespace + "-" + name
+				csr := &certv1beta1.CertificateSigningRequest{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: csrName,
+					},
+				}
+				err := rc.Get(csr)
+				Expect(err).Should(BeNil(), "failed to get csr for %q", csrName)
+				Expect(len(csr.Status.Conditions)).Should(BeEquivalentTo(0), "unexpected status condition")
+
+				csr.Status.Conditions = append(csr.Status.Conditions, certv1beta1.CertificateSigningRequestCondition{
+					Type:           certv1beta1.CertificateApproved,
+					LastUpdateTime: metav1.Now(),
+				})
+
+				csr.Status.Certificate = []byte("Dummy certificate")
+
+				err = rc.Update(csr)
+				Expect(err).Should(BeNil(), "failed to approve CSR")
+			}
+
+			// Reconcile after approved certificates should change the status
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhaseInitializing)
+
+			// Reconcile now should change Phase to running
+			testReconcilePhase(rc, d.nsn, false, false, api.DeploymentPhaseRunning)
+
+			// Ensure both deaemonset and statefulset have default driver name
+			// before editing deployment
+			ds := &appsv1.DaemonSet{}
+			err := rc.Get(ds)
+			Expect(err).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			for _, c := range ds.Spec.Template.Spec.Containers {
+				if c.Name == "pmem-driver" {
+					arg := fmt.Sprintf("-deviceManager=%s", api.DefaultDeviceMode)
+					Expect(c.Args).Should(ContainElement(arg), "mismatched driver device mode")
+					break
+				}
+			}
+
+			// Edit deployment name
+			dep := &api.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      d.nsn.Name,
+					Namespace: d.nsn.Namespace,
+				},
+			}
+			err = rc.Get(dep)
+			Expect(err).Should(BeNil(), "failed to retrieve deployment")
+
+			dep.Spec.DeviceMode = api.DeviceModeDirect
+			err = rc.Update(dep)
+			Expect(err).Should(BeNil(), "failed to update deployment")
+			// Reconcile is expected to fail
+			testReconcilePhase(rc, d.nsn, true, false, api.DeploymentPhaseRunning)
+		})
+
+		It("shall allow to change container resources of a running deployment", func() {
+			d := pmemDeployment{
+				nsn: types.NamespacedName{
+					Name:      "test-update-resource",
+					Namespace: "testnamespace",
+				},
+				controllerCPU:    "100m",
+				controllerMemory: "10Mi",
+				nodeCPU:          "110m",
+				nodeMemory:       "11Mi",
+			}
+
+			rc := getReconciler(getDeployment(d))
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhasePending)
+
+			// Approve certificates
+			for _, name := range []string{"pmem-registry", "pmem-node-controller"} {
+				csrName := d.nsn.Name + "-" + d.nsn.Namespace + "-" + name
+				csr := &certv1beta1.CertificateSigningRequest{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: csrName,
+					},
+				}
+				err := rc.Get(csr)
+				Expect(err).Should(BeNil(), "failed to get csr for %q", csrName)
+				Expect(len(csr.Status.Conditions)).Should(BeEquivalentTo(0), "unexpected status condition")
+
+				csr.Status.Conditions = append(csr.Status.Conditions, certv1beta1.CertificateSigningRequestCondition{
+					Type:           certv1beta1.CertificateApproved,
+					LastUpdateTime: metav1.Now(),
+				})
+
+				csr.Status.Certificate = []byte("Dummy certificate")
+
+				err = rc.Update(csr)
+				Expect(err).Should(BeNil(), "failed to approve CSR")
+			}
+
+			// Reconcile after approved certificates should change the status
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhaseInitializing)
+
+			// Reconcile now should change Phase to running
+			testReconcilePhase(rc, d.nsn, false, false, api.DeploymentPhaseRunning)
+
+			ss := &appsv1.StatefulSet{}
+			err := rc.Get(ss)
+			Expect(err).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			for _, c := range ss.Spec.Template.Spec.Containers {
+				Expect(c.Resources.Requests.Cpu().Cmp(resource.MustParse(d.controllerCPU))).Should(BeZero(), "controller cpu resource requests mismatch")
+				Expect(c.Resources.Requests.Memory().Cmp(resource.MustParse(d.controllerMemory))).Should(BeZero(), "controller memory resource requests mismatch")
+			}
+
+			ds := &appsv1.DaemonSet{}
+			err = rc.Get(ds)
+			Expect(err).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			for _, c := range ds.Spec.Template.Spec.Containers {
+				Expect(c.Resources.Requests.Cpu().Cmp(resource.MustParse(d.nodeCPU))).Should(BeZero(), "node cpu resource requests mismatch")
+				Expect(c.Resources.Requests.Memory().Cmp(resource.MustParse(d.nodeMemory))).Should(BeZero(), "node memory resource requests mismatch")
+			}
+
+			// Update deployment resources
+			d.controllerCPU = "200m"
+			d.controllerMemory = "20Mi"
+			d.nodeCPU = "210m"
+			d.nodeMemory = "21Mi"
+			dep := getDeployment(d)
+			err = rc.Update(dep)
+			Expect(err).Should(BeNil(), "failed to update deployment")
+			// Reconcile is expected not to fail
+			testReconcilePhase(rc, d.nsn, false, false, api.DeploymentPhaseRunning)
+
+			// Recheck the container resources are updated
+			ss = &appsv1.StatefulSet{}
+			err = rc.Get(ss)
+			Expect(err).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			for _, c := range ss.Spec.Template.Spec.Containers {
+				Expect(c.Resources.Requests.Cpu().Cmp(resource.MustParse(d.controllerCPU))).Should(BeZero(), "controller cpu resource requests mismatch")
+				Expect(c.Resources.Requests.Memory().Cmp(resource.MustParse(d.controllerMemory))).Should(BeZero(), "controller memory resource requests mismatch")
+			}
+
+			ds = &appsv1.DaemonSet{}
+			err = rc.Get(ds)
+			Expect(err).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			for _, c := range ds.Spec.Template.Spec.Containers {
+				Expect(c.Resources.Requests.Cpu().Cmp(resource.MustParse(d.nodeCPU))).Should(BeZero(), "node cpu resource requests mismatch")
+				Expect(c.Resources.Requests.Memory().Cmp(resource.MustParse(d.nodeMemory))).Should(BeZero(), "node memory resource requests mismatch")
+			}
+		})
+
+		It("shall allow to change container images of a running deployment", func() {
+			d := pmemDeployment{
+				nsn: types.NamespacedName{
+					Name:      "test-update-images",
+					Namespace: "testnamespace",
+				},
+			}
+
+			rc := getReconciler(getDeployment(d))
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhasePending)
+
+			// Approve certificates
+			for _, name := range []string{"pmem-registry", "pmem-node-controller"} {
+				csrName := d.nsn.Name + "-" + d.nsn.Namespace + "-" + name
+				csr := &certv1beta1.CertificateSigningRequest{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: csrName,
+					},
+				}
+				err := rc.Get(csr)
+				Expect(err).Should(BeNil(), "failed to get csr for %q", csrName)
+				Expect(len(csr.Status.Conditions)).Should(BeEquivalentTo(0), "unexpected status condition")
+
+				csr.Status.Conditions = append(csr.Status.Conditions, certv1beta1.CertificateSigningRequestCondition{
+					Type:           certv1beta1.CertificateApproved,
+					LastUpdateTime: metav1.Now(),
+				})
+
+				csr.Status.Certificate = []byte("Dummy certificate")
+
+				err = rc.Update(csr)
+				Expect(err).Should(BeNil(), "failed to approve CSR")
+			}
+
+			// Reconcile after approved certificates should change the status
+			testReconcilePhase(rc, d.nsn, false, true, api.DeploymentPhaseInitializing)
+
+			// Reconcile now should change Phase to running
+			testReconcilePhase(rc, d.nsn, false, false, api.DeploymentPhaseRunning)
+
+			ss := &appsv1.StatefulSet{}
+			Expect(rc.Get(ss)).Should(BeNil(), "stateful set object is expected on a successful deployment")
+			ds := &appsv1.DaemonSet{}
+			Expect(rc.Get(ds)).Should(BeNil(), "node daemon set object is expected on a successful deployment")
+
+			containers := ss.Spec.Template.Spec.Containers
+			containers = append(containers, ds.Spec.Template.Spec.Containers...)
+			for _, c := range containers {
+				switch c.Name {
+				case "pmem-driver":
+					Expect(c.Image).Should(BeEquivalentTo(api.DefaultDriverImage), "mismatched pmem-csi driver image")
+				case "provisioner":
+					Expect(c.Image).Should(BeEquivalentTo(api.DefaultProvisionerImage), "mismatched provisioner image")
+				case "driver-registar":
+					Expect(c.Image).Should(BeEquivalentTo(api.DefaultRegistrarImage), "mismatched node registart image")
+				}
+			}
+
+			// Update images
+			d.image = "test-pmem-csi-image:v0.0.0"
+			d.provisionerImage = "test-provisioner-image:v0.0.0"
+			d.registrarImage = "test-registrar-image:v0.0.0"
+
+			dep := getDeployment(d)
+			Expect(rc.Update(dep)).Should(BeNil(), "failed to update deployment")
+			testReconcilePhase(rc, d.nsn, false, false, api.DeploymentPhaseRunning)
+
+			ss = &appsv1.StatefulSet{}
+			Expect(rc.Get(ss)).Should(BeNil(), "stateful set object is expected on a successful deployment")
+			ds = &appsv1.DaemonSet{}
+			Expect(rc.Get(ds)).Should(BeNil(), "node daemon set is expected on a successful deployment")
+			// Recheck if the images are updated
+			containers = ss.Spec.Template.Spec.Containers
+			containers = append(containers, ds.Spec.Template.Spec.Containers...)
+			for _, c := range containers {
+				switch c.Name {
+				case "pmem-driver":
+					Expect(c.Image).Should(BeEquivalentTo(d.image), "mismatched pmem-csi driver image")
+				case "provisioner":
+					Expect(c.Image).Should(BeEquivalentTo(d.provisionerImage), "mismatched provisioner image")
+				case "driver-registar":
+					Expect(c.Image).Should(BeEquivalentTo(d.registrarImage), "mismatched node registart image")
+				}
+			}
 		})
 	})
 })
