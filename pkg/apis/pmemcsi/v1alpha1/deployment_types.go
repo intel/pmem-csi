590e3d568e022d7163e4468da38964eb7d3a1b66
Amarnath Valluri <amarnath.valluri@intel.com>
2020-01-21 15:17:40 +0200

    operator/deployment: Reconcile deployment changes
    Changes to Deployment object are possible in any phase of the
    deployment. The controller has to reconcile these changes and make sure
    the valid changes are effected to it's sub-resources and invalid
    changes are rejected by reverting the change from deployment object
    stored at api server.
    
    Example invalid changes are:
    - change of device manager mode of a running deployment
    - change of driver name which conflict with other deployment
    
    Unit tests added to test if the deployment changes are handled properly
    by the controller.

--- pkg/apis/pmemcsi/v1alpha1/deployment_types.go
+++ pkg/apis/pmemcsi/v1alpha1/deployment_types.go
@@ -5,6 +5,7 @@ import (
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // DeviceMode type decleration for allowed driver device managers
@@ -258,6 +259,13 @@ func (d *Deployment) Compare(other *Deployment) map[DeploymentChange]struct{} {
 	}
 
 	return changes
+}
+
+func (d *Deployment) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      d.Name,
+		Namespace: d.Namespace,
+	}
 }
 
 func GetDeploymentCRDSchema() *apiextensions.JSONSchemaProps {
