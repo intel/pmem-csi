cba05b0a68aade7783e175ab579266d2e1405e10
Amarnath Valluri <amarnath.valluri@intel.com>
2020-01-23 09:37:20 +0200

    operator: Added license header to all new source files

--- pkg/apis/pmemcsi/group.go
+++ pkg/apis/pmemcsi/group.go
@@ -1,3 +1,9 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 // Package pmemcsi contains pmemcsi API versions.
 //
 // This file ensures Go source parsers acknowledge the pmemcsi package
