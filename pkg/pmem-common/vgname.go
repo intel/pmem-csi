17c1331d544c7b0ae9a8aee07374a161b3ff62a2
Olev Kartau <olev.kartau@intel.com>
2020-02-18 12:57:47 +0200

    Put back "fsdax" as part of VG names
    The removal of "fsdax" part in VG names which was made
    in connection with "drop sector-mode", introduced
    potential API break if VG name was created before change
    and used with code after change.
    It is safer to put back "fsdax" part of VG name,
    it remains hard-coded in all names now, but still
    indicates correctly that namespaces are in FSDAX mode.

--- pkg/pmem-common/vgname.go
+++ pkg/pmem-common/vgname.go
@@ -11,5 +11,9 @@ import (
 )
 
 func VgName(bus *ndctl.Bus, region *ndctl.Region) string {
-	return bus.DeviceName() + region.DeviceName()
+	// Hard-coded string to indicate all namespaces are in "FSDAX" mode.
+	nsmode := "fsdax"
+	// This is present to avoid API break: names used to indicate nsmode
+	// before the sector-mode support was dropped.
+	return bus.DeviceName() + region.DeviceName() + nsmode
 }
