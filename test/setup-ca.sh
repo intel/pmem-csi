38fa1588055f3036fe7916aea557491fba6035d1
Patrick Ohly <patrick.ohly@intel.com>
2020-02-19 14:39:33 +0100

    setup-ca.sh: improve language and formatting
    The language was a bit inconsistent regarding punctuation and
    spaces. The JSON input for cfssl is more readable when provided
    as here-doc.

--- test/setup-ca.sh
+++ test/setup-ca.sh
@@ -1,28 +1,46 @@
 #!/bin/sh
 
-# Directory to use for storing intermediate files
+# Directory to use for storing intermediate files.
 CA=${CA:="pmem-ca"}
 WORKDIR=${WORKDIR:-$(mktemp -d -u -t pmem-XXXX)}
 mkdir -p $WORKDIR
 cd $WORKDIR
 
-#download cfssl utilities
+# Check for cfssl utilities.
 cfssl_found=1
 (command -v cfssl 2>&1 >/dev/null && command -v cfssljson 2>&1 >/dev/null) || cfssl_found=0
 if [ $cfssl_found -eq 0 ]; then
-    echo "cfssl tools not found, Please install cfssl, cfssljson."
+    echo "cfssl tools not found, Please install cfssl and cfssljson."
     exit 1
 fi
 
-#Generate CA certificates
-echo '{"CN": "'$CA'", "key": {"algo": "rsa", "size": 2048}}' | cfssl gencert -initca - | cfssljson -bare ca
+# Generate CA certificates.
+<<EOF cfssl gencert -initca - | cfssljson -bare ca
+{
+    "CN": "$CA",
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    }
+}
+EOF
 
-# Generate server and client certificates
+# Generate server and client certificates.
 DEFAULT_CNS="pmem-registry pmem-node-controller"
 CNS="${DEFAULT_CNS} ${EXTRA_CNS:=""}"
 for name in ${CNS}; do
-  echo '{"CN": "'$name'", "key": {"algo": "ecdsa", "size": 256}}' | \
-  cfssl gencert -ca=ca.pem -ca-key=ca-key.pem - | cfssljson -bare $name
+  <<EOF cfssl gencert -ca=ca.pem -ca-key=ca-key.pem - | cfssljson -bare $name
+{
+    "CN": "$name",
+    "hosts": [
+        "$name"
+    ],
+    "key": {
+        "algo": "ecdsa",
+        "size": 256
+    }
+}
+EOF
 done
 
 echo "Generated certificates can be located at: $WORKDIR"
