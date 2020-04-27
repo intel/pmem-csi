edabcfe491e27efd1cbb5cd8aa07cb7c732ecce5
Patrick Ohly <patrick.ohly@intel.com>
2020-04-27 21:59:25 +0200

    operator: also validate objects deployed by framework

--- test/e2e/operator/driver.go
+++ test/e2e/operator/driver.go
@@ -7,7 +7,13 @@ SPDX-License-Identifier: Apache-2.0
 package operator
 
 import (
+	"context"
+	"time"
+
 	"github.com/intel/pmem-csi/test/e2e/deploy"
+	"github.com/intel/pmem-csi/test/e2e/operator/validate"
+
+	"k8s.io/kubernetes/test/e2e/framework"
 
 	. "github.com/onsi/ginkgo"
 )
@@ -18,7 +24,15 @@ var _ = deploy.DescribeForSome("driver", func(d *deploy.Deployment) bool {
 	return d.HasOperator && d.HasDriver
 }, func(d *deploy.Deployment) {
 
+	f := framework.NewDefaultFramework("driver")
+	f.SkipNamespaceCreation = true
+
 	It("runs", func() {
 		// Once we get here, the deploy package has already checked for us that the driver is operational.
+		// We can verify that it meets the spec.
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+		defer cancel()
+		validate.DriverDeployment(ctx, f, d, d.GetDriverDeployment())
 	})
 })
