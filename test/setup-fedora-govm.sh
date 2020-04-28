eacac37e999a4a0ed33c4aee9ba5caa7e916b104
Olev Kartau <olev.kartau@intel.com>
2020-04-28 12:12:43 +0300

    Fedora VM setup: use quiet option in "dnf update"
    On a VM using cloud image, packages installation produces
    huge amount (thousands) of lines of locale-related warnings:
    warning: file /usr/share/locale/XYZ: remove failed: No such file or directory.
    Add -q option to avoid those. With quiet option, we also
    miss exact versions of packages updated, but I think we don't
    need that information in job logs.

--- test/setup-fedora-govm.sh
+++ test/setup-fedora-govm.sh
@@ -85,9 +85,9 @@ while ! dnf install -y $packages; do
     fi
     cnt=$(($cnt + 1))
     # If it works, proceed immediately. If it fails, sleep and try again without aborting on an error.
-    if ! dnf update -y --refresh; then
+    if ! dnf update -q -y --refresh; then
         sleep 20
-        dnf update -y --refresh || true
+        dnf update -q -y --refresh || true
     fi
 done
 
