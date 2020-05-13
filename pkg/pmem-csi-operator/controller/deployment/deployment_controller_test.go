de8b523c0c47753e374bb433fed3f16d5fcea489
Patrick Ohly <patrick.ohly@intel.com>
2020-05-13 15:51:58 +0200

    operator: test deployment in parallel
    This replaces Gomega+Ginkgo with plain recursive Go tests and testify
    for the asserts. The main advantage is that now tests automatically
    run in parallel through goroutines, which reduces the runtime from
    over 10 minutes (not sure how much because it timed out) to something
    over two minutes on my desktop machine.
    
    Logging output continues to be problematic because it is not related
    to the test that triggers it.

--- pkg/pmem-csi-operator/controller/deployment/deployment_controller_test.go
+++ pkg/pmem-csi-operator/controller/deployment/deployment_controller_test.go
@@ -11,8 +11,8 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/intel/pmem-csi/deploy"
 	"github.com/intel/pmem-csi/pkg/apis"
@@ -36,15 +36,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
-
-func TestDeploymentController(t *testing.T) {
-	RegisterFailHandler(Fail)
-
-	err := apis.AddToScheme(scheme.Scheme)
-	Expect(err).Should(BeNil(), "Failed to add api schema")
-
-	RunSpecs(t, "PMEM Operator API test suite")
-}
 
 type pmemDeployment struct {
 	name                                                string
@@ -104,18 +95,18 @@ func getDeployment(d *pmemDeployment) *api.Deployment {
 	return dep
 }
 
-func testDeploymentPhase(c client.Client, name string, expectedPhase api.DeploymentPhase) {
+func testDeploymentPhase(t *testing.T, c client.Client, name string, expectedPhase api.DeploymentPhase) {
 	depObject := &api.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
 		},
 	}
-	err := c.Get(context.TODO(), namespacedNameWithOffset(3, depObject), depObject)
-	ExpectWithOffset(2, err).Should(BeNil(), "failed to retrive deployment object")
-	ExpectWithOffset(2, depObject.Status.Phase).Should(BeEquivalentTo(expectedPhase), "Unexpected status phase")
+	err := c.Get(context.TODO(), namespacedNameWithOffset(t, 3, depObject), depObject)
+	require.NoError(t, err, "failed to retrive deployment object")
+	require.Equal(t, expectedPhase, depObject.Status.Phase, "Unexpected status phase")
 }
 
-func testReconcile(rc reconcile.Reconciler, name string, expectErr bool, expectedRequeue bool) {
+func testReconcile(t *testing.T, rc reconcile.Reconciler, name string, expectErr bool, expectedRequeue bool) {
 	req := reconcile.Request{
 		NamespacedName: types.NamespacedName{
 			Name: name,
@@ -123,25 +114,25 @@ func testReconcile(rc reconcile.Reconciler, name string, expectErr bool, expecte
 	}
 	resp, err := rc.Reconcile(req)
 	if expectErr {
-		ExpectWithOffset(2, err).ShouldNot(BeNil(), "expected reconcile failure")
+		require.Error(t, err, "expected reconcile failure")
 	} else {
-		ExpectWithOffset(2, err).Should(BeNil(), "reconcile failed with error")
+		require.NoError(t, err, "reconcile failed with error")
 	}
-	ExpectWithOffset(2, resp.Requeue).Should(BeEquivalentTo(expectedRequeue), "expected requeue reconcile")
+	require.Equal(t, expectedRequeue, resp.Requeue, "expected requeue reconcile")
 }
 
-func testReconcilePhase(rc reconcile.Reconciler, c client.Client, name string, expectErr bool, expectedRequeue bool, expectedPhase api.DeploymentPhase) {
-	testReconcile(rc, name, expectErr, expectedRequeue)
-	testDeploymentPhase(c, name, expectedPhase)
+func testReconcilePhase(t *testing.T, rc reconcile.Reconciler, c client.Client, name string, expectErr bool, expectedRequeue bool, expectedPhase api.DeploymentPhase) {
+	testReconcile(t, rc, name, expectErr, expectedRequeue)
+	testDeploymentPhase(t, c, name, expectedPhase)
 }
 
-func namespacedName(obj runtime.Object) types.NamespacedName {
-	return namespacedNameWithOffset(2, obj)
+func namespacedName(t *testing.T, obj runtime.Object) types.NamespacedName {
+	return namespacedNameWithOffset(t, 2, obj)
 }
 
-func namespacedNameWithOffset(offset int, obj runtime.Object) types.NamespacedName {
+func namespacedNameWithOffset(t *testing.T, offset int, obj runtime.Object) types.NamespacedName {
 	metaObj, err := meta.Accessor(obj)
-	ExpectWithOffset(offset, err).Should(BeNil(), "failed to get accessor")
+	require.NoError(t, err, "failed to get accessor")
 
 	return types.NamespacedName{Name: metaObj.GetName(), Namespace: metaObj.GetNamespace()}
 }
@@ -164,7 +155,6 @@ func deleteDeployment(c client.Client, name, ns string) error {
 		return err
 	}
 
-	By(fmt.Sprintf("Deleting Deployment '%s'", dep.Name))
 	if err := c.Delete(context.TODO(), dep); err != nil {
 		return err
 	}
@@ -178,53 +168,63 @@ func deleteDeployment(c client.Client, name, ns string) error {
 			Name: dep.Name,
 		},
 	}
-	By(fmt.Sprintf("Deleting csi driver '%s'", driver.Name))
 	return c.Delete(context.TODO(), driver)
 }
 
-var _ = Describe("Operator", func() {
-	testIt := func(testK8sVersion version.Version) {
-		var c client.Client
-		var rc reconcile.Reconciler
-		var testNamespace = "test-namespace"
-		var testDriverImage = "fake-driver-image"
+func TestDeploymentController(t *testing.T) {
+	err := apis.AddToScheme(scheme.Scheme)
+	require.NoError(t, err, "add api schema")
 
-		BeforeEach(func() {
-			c = fake.NewFakeClient()
-			var err error
-			rc, err = deployment.NewReconcileDeployment(c, pmemcontroller.ControllerOptions{
+	testIt := func(t *testing.T, testK8sVersion version.Version) {
+		type testContext struct {
+			c  client.Client
+			rc reconcile.Reconciler
+		}
+		const (
+			testNamespace   = "test-namespace"
+			testDriverImage = "fake-driver-image"
+		)
+
+		setup := func(t *testing.T) testContext {
+			c := fake.NewFakeClient()
+			rc, err := deployment.NewReconcileDeployment(c, pmemcontroller.ControllerOptions{
 				Namespace:   testNamespace,
 				K8sVersion:  testK8sVersion,
 				DriverImage: testDriverImage,
 			})
-			Expect(err).ShouldNot(HaveOccurred(), "create new reconciler")
-		})
+			require.NoError(t, err, "create new reconciler")
+			return testContext{c, rc}
+		}
 
-		validateDriver := func(dep *api.Deployment) {
+		validateDriver := func(t *testing.T, tc testContext, dep *api.Deployment) {
 			// We may have to fill in some defaults, so make a copy first.
 			dep = dep.DeepCopyObject().(*api.Deployment)
 			if dep.Spec.Image == "" {
 				dep.Spec.Image = testDriverImage
 			}
 
-			ExpectWithOffset(1, validate.DriverDeployment(c, testK8sVersion, testNamespace, *dep)).ShouldNot(HaveOccurred(), "validate deployment")
+			require.NoError(t, validate.DriverDeployment(tc.c, testK8sVersion, testNamespace, *dep), "validate deployment")
 		}
 
-		It("shall allow deployment with defaults", func() {
+		t.Parallel()
+
+		t.Run("deployment with defaults", func(t *testing.T) {
+			tc := setup(t)
 			d := &pmemDeployment{
 				name: "test-deployment",
 			}
 
 			dep := getDeployment(d)
 
-			err := c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment")
+			err := tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment")
 
-			testReconcilePhase(rc, c, d.name, false, false, api.DeploymentPhaseRunning)
-			validateDriver(dep)
+			testReconcilePhase(t, tc.rc, tc.c, d.name, false, false, api.DeploymentPhaseRunning)
+			validateDriver(t, tc, dep)
 		})
 
-		It("shall allow deployment with explicit values", func() {
+		t.Run("deployment with explicit values", func(t *testing.T) {
+			tc := setup(t)
 			d := &pmemDeployment{
 				name:             "test-deployment",
 				image:            "test-driver:v0.0.0",
@@ -239,15 +239,16 @@ var _ = Describe("Operator", func() {
 			}
 
 			dep := getDeployment(d)
-			err := c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment")
+			err := tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment")
 
 			// Reconcile now should change Phase to running
-			testReconcilePhase(rc, c, d.name, false, false, api.DeploymentPhaseRunning)
-			validateDriver(dep)
+			testReconcilePhase(t, tc.rc, tc.c, d.name, false, false, api.DeploymentPhaseRunning)
+			validateDriver(t, tc, dep)
 		})
 
-		It("shall allow multiple deployments", func() {
+		t.Run("multiple deployments", func(t *testing.T) {
+			tc := setup(t)
 			d1 := &pmemDeployment{
 				name: "test-deployment1",
 			}
@@ -257,19 +258,19 @@ var _ = Describe("Operator", func() {
 			}
 
 			dep := getDeployment(d1)
-			err := c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment1")
+			err := tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment1")
 
 			dep = getDeployment(d2)
-			err = c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment2")
+			err = tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment2")
 
-			testReconcilePhase(rc, c, d1.name, false, false, api.DeploymentPhaseRunning)
-			testReconcilePhase(rc, c, d2.name, false, false, api.DeploymentPhaseRunning)
-
+			testReconcilePhase(t, tc.rc, tc.c, d1.name, false, false, api.DeploymentPhaseRunning)
+			testReconcilePhase(t, tc.rc, tc.c, d2.name, false, false, api.DeploymentPhaseRunning)
 		})
 
-		It("shall not allow invalid device mode", func() {
+		t.Run("invalid device mode", func(t *testing.T) {
+			tc := setup(t)
 			d := &pmemDeployment{
 				name:       "test-driver-modes",
 				deviceMode: "foobar",
@@ -277,16 +278,17 @@ var _ = Describe("Operator", func() {
 
 			dep := getDeployment(d)
 
-			err := c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment")
+			err := tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment")
 			// Deployment should failed with an error
-			testReconcilePhase(rc, c, d.name, true, false, api.DeploymentPhaseFailed)
+			testReconcilePhase(t, tc.rc, tc.c, d.name, true, false, api.DeploymentPhaseFailed)
 		})
 
-		It("shall use provided private keys", func() {
+		t.Run("provided private keys", func(t *testing.T) {
+			tc := setup(t)
 			// Generate private key
 			regKey, err := pmemtls.NewPrivateKey()
-			Expect(err).Should(BeNil(), "Failed to generate a private key: %v", err)
+			require.NoError(t, err, "Failed to generate a private key: %v", err)
 
 			encodedKey := pmemtls.EncodeKey(regKey)
 
@@ -295,27 +297,28 @@ var _ = Describe("Operator", func() {
 				regKey: encodedKey,
 			}
 			dep := getDeployment(d)
-			err = c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment")
+			err = tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment")
 
 			// First deployment expected to be successful
-			testReconcilePhase(rc, c, d.name, false, false, api.DeploymentPhaseRunning)
-			validateDriver(dep)
+			testReconcilePhase(t, tc.rc, tc.c, d.name, false, false, api.DeploymentPhaseRunning)
+			validateDriver(t, tc, dep)
 		})
 
-		It("shall use provided private keys and certificates", func() {
+		t.Run("provided private keys and certificates", func(t *testing.T) {
+			tc := setup(t)
 			ca, err := pmemtls.NewCA(nil, nil)
-			Expect(err).Should(BeNil(), "faield to instantiate CA")
+			require.NoError(t, err, "faield to instantiate CA")
 
 			regKey, err := pmemtls.NewPrivateKey()
-			Expect(err).Should(BeNil(), "failed to generate a private key: %v", err)
+			require.NoError(t, err, "failed to generate a private key: %v", err)
 			regCert, err := ca.GenerateCertificate("pmem-registry", regKey.Public())
-			Expect(err).Should(BeNil(), "failed to sign registry key")
+			require.NoError(t, err, "failed to sign registry key")
 
 			ncKey, err := pmemtls.NewPrivateKey()
-			Expect(err).Should(BeNil(), "failed to generate a private key: %v", err)
+			require.NoError(t, err, "failed to generate a private key: %v", err)
 			ncCert, err := ca.GenerateCertificate("pmem-node-controller", ncKey.Public())
-			Expect(err).Should(BeNil(), "failed to sign node controller key")
+			require.NoError(t, err, "failed to sign node controller key")
 
 			d := &pmemDeployment{
 				name:    "test-deployment",
@@ -326,27 +329,28 @@ var _ = Describe("Operator", func() {
 				ncCert:  pmemtls.EncodeCert(ncCert),
 			}
 			dep := getDeployment(d)
-			err = c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment")
+			err = tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment")
 
 			// First deployment expected to be successful
-			testReconcilePhase(rc, c, d.name, false, false, api.DeploymentPhaseRunning)
-			validateDriver(dep)
+			testReconcilePhase(t, tc.rc, tc.c, d.name, false, false, api.DeploymentPhaseRunning)
+			validateDriver(t, tc, dep)
 		})
 
-		It("shall detect invalid private keys and certificates", func() {
+		t.Run("invalid private keys and certificates", func(t *testing.T) {
+			tc := setup(t)
 			ca, err := pmemtls.NewCA(nil, nil)
-			Expect(err).Should(BeNil(), "faield to instantiate CA")
+			require.NoError(t, err, "faield to instantiate CA")
 
 			regKey, err := pmemtls.NewPrivateKey()
-			Expect(err).Should(BeNil(), "failed to generate a private key: %v", err)
+			require.NoError(t, err, "failed to generate a private key: %v", err)
 			regCert, err := ca.GenerateCertificate("invalid-registry", regKey.Public())
-			Expect(err).Should(BeNil(), "failed to sign registry key")
+			require.NoError(t, err, "failed to sign registry key")
 
 			ncKey, err := pmemtls.NewPrivateKey()
-			Expect(err).Should(BeNil(), "failed to generate a private key: %v", err)
+			require.NoError(t, err, "failed to generate a private key: %v", err)
 			ncCert, err := ca.GenerateCertificate("invalid-node-controller", ncKey.Public())
-			Expect(err).Should(BeNil(), "failed to sign node key")
+			require.NoError(t, err, "failed to sign node key")
 
 			d := &pmemDeployment{
 				name:    "test-deployment-cert-invalid",
@@ -357,28 +361,29 @@ var _ = Describe("Operator", func() {
 				ncCert:  pmemtls.EncodeCert(ncCert),
 			}
 			dep := getDeployment(d)
-			err = c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment")
+			err = tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment")
 
-			testReconcilePhase(rc, c, d.name, true, true, api.DeploymentPhaseFailed)
+			testReconcilePhase(t, tc.rc, tc.c, d.name, true, true, api.DeploymentPhaseFailed)
 		})
 
-		It("shall detect expired certificates", func() {
+		t.Run("expired certificates", func(t *testing.T) {
+			tc := setup(t)
 			oneDayAgo := time.Now().Add(-24 * time.Hour)
 			oneMinuteAgo := time.Now().Add(-1 * time.Minute)
 
 			ca, err := pmemtls.NewCA(nil, nil)
-			Expect(err).Should(BeNil(), "faield to instantiate CA")
+			require.NoError(t, err, "faield to instantiate CA")
 
 			regKey, err := pmemtls.NewPrivateKey()
-			Expect(err).Should(BeNil(), "failed to generate a private key: %v", err)
+			require.NoError(t, err, "failed to generate a private key: %v", err)
 			regCert, err := ca.GenerateCertificateWithDuration("pmem-registry", oneDayAgo, oneMinuteAgo, regKey.Public())
-			Expect(err).Should(BeNil(), "failed to registry sign key")
+			require.NoError(t, err, "failed to registry sign key")
 
 			ncKey, err := pmemtls.NewPrivateKey()
-			Expect(err).Should(BeNil(), "failed to generate a private key: %v", err)
+			require.NoError(t, err, "failed to generate a private key: %v", err)
 			ncCert, err := ca.GenerateCertificateWithDuration("pmem-node-controller", oneDayAgo, oneMinuteAgo, ncKey.Public())
-			Expect(err).Should(BeNil(), "failed to sign node controller key")
+			require.NoError(t, err, "failed to sign node controller key")
 
 			d := &pmemDeployment{
 				name:    "test-deployment-cert-expired",
@@ -389,62 +394,65 @@ var _ = Describe("Operator", func() {
 				ncCert:  pmemtls.EncodeCert(ncCert),
 			}
 			dep := getDeployment(d)
-			err = c.Create(context.TODO(), dep)
-			Expect(err).Should(BeNil(), "failed to create deployment")
+			err = tc.c.Create(context.TODO(), dep)
+			require.NoError(t, err, "failed to create deployment")
 
-			testReconcilePhase(rc, c, d.name, true, true, api.DeploymentPhaseFailed)
+			testReconcilePhase(t, tc.rc, tc.c, d.name, true, true, api.DeploymentPhaseFailed)
 		})
 
-		Context("shall support updating", func() {
+		t.Run("updating", func(t *testing.T) {
+			t.Parallel()
 			for _, testcase := range testcases.UpdateTests() {
 				testcase := testcase
-				Context(testcase.Name, func() {
+				t.Run(testcase.Name, func(t *testing.T) {
 					testIt := func(restart bool) {
+						tc := setup(t)
 						dep := testcase.Deployment.DeepCopyObject().(*api.Deployment)
 
 						// When working with the fake client, we need to make up a UID.
 						dep.UID = types.UID("fake-uid-" + dep.Name)
 
-						err := c.Create(context.TODO(), dep)
-						Expect(err).ShouldNot(HaveOccurred(), "create deployment")
+						err := tc.c.Create(context.TODO(), dep)
+						require.NoError(t, err, "create deployment")
 
-						testReconcilePhase(rc, c, dep.Name, false, false, api.DeploymentPhaseRunning)
-						validateDriver(dep)
+						testReconcilePhase(t, tc.rc, tc.c, dep.Name, false, false, api.DeploymentPhaseRunning)
+						validateDriver(t, tc, dep)
 
 						// Reconcile now should keep phase as running.
-						testReconcilePhase(rc, c, dep.Name, false, false, api.DeploymentPhaseRunning)
+						testReconcilePhase(t, tc.rc, tc.c, dep.Name, false, false, api.DeploymentPhaseRunning)
 
 						// Retrieve existing object before updating it.
-						err = c.Get(context.TODO(), types.NamespacedName{Name: dep.Name}, dep)
-						Expect(err).ShouldNot(HaveOccurred(), "retrive existing deployment object")
+						err = tc.c.Get(context.TODO(), types.NamespacedName{Name: dep.Name}, dep)
+						require.NoError(t, err, "retrive existing deployment object")
 
 						if restart {
 							// Simulate restarting the operator by creating a new instance.
-							rc, err = deployment.NewReconcileDeployment(c, pmemcontroller.ControllerOptions{
+							rc, err := deployment.NewReconcileDeployment(tc.c, pmemcontroller.ControllerOptions{
 								Namespace:   testNamespace,
 								K8sVersion:  testK8sVersion,
 								DriverImage: testDriverImage,
 							})
-							Expect(err).ShouldNot(HaveOccurred(), "recreate reconciler")
+							require.NoError(t, err, "recreate reconciler")
+							tc.rc = rc
 						}
 
 						// Update.
 						testcase.Mutate(dep)
-						err = c.Update(context.TODO(), dep)
-						Expect(err).ShouldNot(HaveOccurred(), "update deployment")
+						err = tc.c.Update(context.TODO(), dep)
+						require.NoError(t, err, "update deployment")
 
 						// Reconcile is expected to not fail.
-						testReconcilePhase(rc, c, dep.Name, false, false, api.DeploymentPhaseRunning)
+						testReconcilePhase(t, tc.rc, tc.c, dep.Name, false, false, api.DeploymentPhaseRunning)
 
 						// Recheck the container resources are updated
-						validateDriver(dep)
+						validateDriver(t, tc, dep)
 					}
 
-					It("while running", func() {
+					t.Run("while running", func(t *testing.T) {
 						testIt(false)
 					})
 
-					It("while stopped", func() {
+					t.Run("while stopped", func(t *testing.T) {
 						testIt(true)
 					})
 				})
@@ -452,11 +460,13 @@ var _ = Describe("Operator", func() {
 		})
 	}
 
+	t.Parallel()
+
 	// Validate for all supported Kubernetes versions.
 	for _, yaml := range deploy.ListAll() {
 		version := yaml.Kubernetes
-		Context(fmt.Sprintf("Kubernetes %v", version), func() {
-			testIt(version)
+		t.Run(fmt.Sprintf("Kubernetes %v", version), func(t *testing.T) {
+			testIt(t, version)
 		})
 	}
-})
+}
