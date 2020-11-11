fba7b1a0904b3f275410b3d963becf1593b4ab48
Patrick Ohly <patrick.ohly@intel.com>
2020-11-11 11:38:10 +0100

    pmem-csi-driver test: enable klog/v2
    To see log output, we must initialize the arguments and run the test
    with -v=5.

--- pkg/pmem-csi-driver/pmem-csi-driver_test.go
+++ pkg/pmem-csi-driver/pmem-csi-driver_test.go
@@ -15,6 +15,8 @@ import (
 	"os"
 	"testing"
 
+	"k8s.io/klog/v2"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -24,6 +26,10 @@ var (
 	certFile = os.ExpandEnv("${TEST_WORK}/pmem-ca/pmem-registry.pem")
 	keyFile  = os.ExpandEnv("${TEST_WORK}/pmem-ca/pmem-registry-key.pem")
 )
+
+func init() {
+	klog.InitFlags(nil)
+}
 
 func TestMetrics(t *testing.T) {
 	cases := map[string]struct {
