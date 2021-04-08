833877f90f969852619aae99caa1d7f94e643d09
Amarnath Valluri <amarnath.valluri@intel.com>
2021-04-08 15:52:50 +0300

    operator: version skew tests
    Added both upgrade and downgrade operator tests. These are similar to
    driver version skew tests. We test that the running driver deployments
    using one version keep runs even after upgrading/downgrading the
    operator.
    
    Currently, these tests are limited to running the operator deployed via
    YAML files not using the OLM. Because there is no way to keep the CRD
    while deleting operator using olm.

---
+++ test/e2e/versionskew/operator.go
@@ -0,0 +1,215 @@
+/*
+Copyright 2020 Intel Corporation.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package versionskew
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	api "github.com/intel/pmem-csi/pkg/apis/pmemcsi/v1beta1"
+	"github.com/intel/pmem-csi/test/e2e/deploy"
+	"golang.org/x/net/context"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/kubernetes/test/e2e/framework"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = deploy.DescribeForSome("versionskew", func(d *deploy.Deployment) bool {
+	// Run these tests only for bare operator deployments, not for the operator
+	// deployed by OLM. Because there is no way how to ask olm to stop the operator
+	// but keep the CRD.
+	return d.HasOperator && !d.HasDriver && !d.HasOLM
+}, func(d *deploy.Deployment) {
+	var (
+		evWatcher  watch.Interface
+		evWait     sync.WaitGroup
+		evCaptured map[types.UID]bool
+		evMutex    sync.Mutex
+	)
+
+	f := framework.NewDefaultFramework("skew")
+	f.SkipNamespaceCreation = true
+
+	BeforeEach(func() {
+		var err error
+
+		Expect(f).ShouldNot(BeNil(), "framework init")
+
+		evWatcher, err = f.ClientSet.CoreV1().Events("").Watch(context.TODO(), metav1.ListOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "get events watcher")
+
+		evMutex = sync.Mutex{}
+	})
+
+	deployDriver := func(name, image string) api.PmemCSIDeployment {
+		cr := api.PmemCSIDeployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+			Spec: api.DeploymentSpec{
+				DeviceMode: api.DeviceModeDirect,
+			},
+		}
+		if image != "" {
+			cr.Spec.Image = image
+		}
+		driverDep := deploy.CreateDeploymentCR(f, cr)
+		return driverDep
+	}
+
+	changeVersion := func(version string) {
+		newDeploymentName := d.Name()
+		if version != "" {
+			// deploy.Parse() expects the deployment is seperated with '-'
+			// and the version should be the 4th field.
+			newDeploymentName = d.Name() + "---" + version
+		}
+
+		// Change operator release.
+		By(fmt.Sprintf("Switching the operator to %s", newDeploymentName))
+		deployment, err := deploy.Parse(newDeploymentName)
+		framework.ExpectNoError(err, "internal error while parsing %s: %v", newDeploymentName, err)
+		deploy.EnsureDeploymentNow(f, deployment)
+	}
+
+	clearEvents := func() {
+		evMutex.Lock()
+		defer evMutex.Unlock()
+		evCaptured = map[types.UID]bool{}
+	}
+
+	captureEvents := func() {
+		// Initialize events map
+		clearEvents()
+
+		timeStamp := time.Now()
+		evWait.Add(1)
+		go func() {
+			defer evWait.Done()
+			for watchEvent := range evWatcher.ResultChan() {
+				ev, ok := watchEvent.Object.(*corev1.Event)
+				if !ok || !ev.LastTimestamp.After(timeStamp) ||
+					ev.Source.Component != "pmem-csi-operator" ||
+					ev.Reason != api.EventReasonRunning {
+					continue
+				}
+				evMutex.Lock()
+				evCaptured[ev.InvolvedObject.UID] = true
+				evMutex.Unlock()
+			}
+		}()
+	}
+
+	stopEventCapture := func() {
+		evWatcher.Stop()
+	}
+
+	pmemImage := func(containers []corev1.Container) string {
+		for _, c := range containers {
+			if c.Name == "pmem-driver" || c.Name == "pmem-csi-operator" {
+				return c.Image
+			}
+		}
+		return ""
+	}
+
+	getOperatorImage := func() string {
+		dep, err := f.ClientSet.AppsV1().Deployments(d.Namespace).Get(context.TODO(), "pmem-csi-operator", metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "get operator deployment")
+
+		return pmemImage(dep.Spec.Template.Spec.Containers)
+	}
+
+	ensureDeploymentReady := func(uids []types.UID, what string) {
+		framework.Logf("Ensuring that the CRs got reconciled")
+
+		Eventually(func() bool {
+			evMutex.Lock()
+			defer evMutex.Unlock()
+			for _, uid := range uids {
+				if !evCaptured[uid] {
+					return false
+				}
+			}
+			return true
+		}, "5m", "2s").Should(BeTrue(), "%s: reconcile driver deployments:", what)
+	}
+
+	testVersion := func(toVer string, what string) {
+		// Default driver image expected on post version change
+		defaultDriverImage := ""
+		// Custom image explicitly chosen by the test
+		// It must be locked to current operator version
+		customImage := getOperatorImage()
+
+		if toVer != "" { // downgrade
+			// Downgrading to base
+			defaultDriverImage = "intel/pmem-csi-driver:v" + toVer
+		} else { // upgrade
+			defaultDriverImage = os.Getenv("PMEM_CSI_IMAGE")
+		}
+
+		driverCR1 := deployDriver(what+"-default-image", "")
+		defer deploy.DeleteDeploymentCR(f, driverCR1.Name)
+
+		driverCR2 := deployDriver(what+"-custom-image", customImage)
+		defer deploy.DeleteDeploymentCR(f, driverCR2.Name)
+
+		uids := []types.UID{driverCR1.GetUID(), driverCR2.GetUID()}
+
+		framework.Logf("Checking for events on: %s", uids)
+		captureEvents()
+		defer stopEventCapture()
+
+		// Ensure the operator served the deployments
+		ensureDeploymentReady(uids, fmt.Sprintf("before version %s", what))
+		framework.Logf("Deployed both the drivers. Changing operator version to: %q", toVer)
+
+		clearEvents()
+
+		changeVersion(toVer)
+		framework.Logf("%s: Operator changed to: %s", what, base)
+
+		// Ensure the deployments reconciled with changed operator
+		ensureDeploymentReady(uids, fmt.Sprintf("after version %s", what))
+
+		ss, err := f.ClientSet.AppsV1().StatefulSets(d.Namespace).Get(context.TODO(), driverCR1.ControllerDriverName(), metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "%s: get driver satefuleset", what)
+		Expect(pmemImage(ss.Spec.Template.Spec.Containers)).To(HavePrefix(defaultDriverImage), "controller uses %sed driver image", what)
+
+		ds, err := f.ClientSet.AppsV1().DaemonSets(d.Namespace).Get(context.TODO(), driverCR1.NodeDriverName(), metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "%s: get driver daemonset", what)
+		Expect(pmemImage(ds.Spec.Template.Spec.Containers)).To(HavePrefix(defaultDriverImage), "node driver uses %sed driver image", what)
+
+		ss, err = f.ClientSet.AppsV1().StatefulSets(d.Namespace).Get(context.TODO(), driverCR2.ControllerDriverName(), metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "%s: get driver satefuleset", what)
+		Expect(pmemImage(ss.Spec.Template.Spec.Containers)).To(BeEquivalentTo(customImage), "controller uses custom driver image")
+
+		ds, err = f.ClientSet.AppsV1().DaemonSets(d.Namespace).Get(context.TODO(), driverCR2.NodeDriverName(), metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "%s: get driver daemonset", what)
+		Expect(pmemImage(ds.Spec.Template.Spec.Containers)).To(BeEquivalentTo(customImage), "node driver uses custom driver image")
+	}
+
+	It("downgrade [Slow]", func() {
+		testVersion(base, "downgrade")
+	})
+
+	It("upgrade [Slow]", func() {
+		// downgrade the operator to base version
+		changeVersion(base)
+
+		// And test upgrade
+		testVersion("" /*devel*/, "upgrade")
+	})
+})
